f6e0b2c2d24203f28bdf7d64fcb5530281858593
Martin <martin.geor04@gmail.com>
2023-09-19 21:34:02 +0300

    Add project

---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-Exercise/01.Ages/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string person;
+
+            if (age <= 2)
+                person = "baby";
+            else if (age >= 3 && age <= 13)
+                person = "child";
+            else if (age >= 14 && age <= 19)
+                person = "teenager";
+            else if (age >= 20 && age <= 65)
+                person = "adult";
+            else
+                person = "elder";
+
+            Console.WriteLine(person);
+        }       
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-Exercise/05.Login/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _05.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loggedIn = false;
+
+            string usernameInput = Console.ReadLine();
+            string passwordInput = Console.ReadLine();
+            string correctPassword = String.Empty;
+
+            for (int i = passwordInput.Length; i > 0; i--)
+            {
+
+            }
+
+            while (!loggedIn)
+            {
+            }
+        }
+    }
+}

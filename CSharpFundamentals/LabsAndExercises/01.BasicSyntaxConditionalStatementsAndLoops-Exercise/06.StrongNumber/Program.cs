ac31ff7b4c5db4a59683e6157f52ea3c1d90cfd1
Martin <martin.geor04@gmail.com>
2023-09-20 00:07:44 +0300

    Completed 06

---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _06.StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            int n = inputNum; 
+            int sum = 0;
+
+            for (int i = 0; i < inputNum.ToString().Length; i++)
+            {
+                int lastDigit = n % 10;
+                int factorial = 1;
+
+                // Get factorial
+                for (int j = lastDigit; j > 0; j--)
+                {
+                    factorial = factorial * j;
+                }
+                sum += factorial;
+
+                n = n / 10;
+            }
+
+            if (inputNum == sum)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}

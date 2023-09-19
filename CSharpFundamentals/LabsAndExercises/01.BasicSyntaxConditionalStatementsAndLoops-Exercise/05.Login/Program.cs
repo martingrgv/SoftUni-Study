54d3e90a6b72f6ab26a5c6ebbd1d702c5ee411a7
Martin <martin.geor04@gmail.com>
2023-09-19 23:13:32 +0300

    Completed 05

--- CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-Exercise/05.Login/Program.cs
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-Exercise/05.Login/Program.cs
@@ -7,18 +7,40 @@ namespace _05.Login
         static void Main(string[] args)
         {
             bool loggedIn = false;
-
+            bool blockedUser = false;
+            int attempts = 0;
             string usernameInput = Console.ReadLine();
-            string passwordInput = Console.ReadLine();
             string correctPassword = String.Empty;
 
-            for (int i = passwordInput.Length; i > 0; i--)
+            // Reverse username as password
+            for (int i = usernameInput.Length; i > 0; i--)
             {
-
+                correctPassword += usernameInput[i - 1];
             }
 
-            while (!loggedIn)
+            while (!loggedIn && !blockedUser)
             {
+                string passwordInput = Console.ReadLine();
+
+                if (passwordInput == correctPassword) // if password correct print login
+                {
+                    Console.WriteLine("User {0} logged in.", usernameInput);
+                    loggedIn = true;
+                    break;
+                }
+                else
+                {
+                    if (attempts < 3) // cup attempts
+                    {
+                        attempts++;
+                        Console.WriteLine("Incorrect password. Try again.");
+                    }
+                    else // block user
+                    {
+                        blockedUser = true;
+                        Console.WriteLine("User {0} blocked!", usernameInput);
+                    }
+                }
             }
         }
     }

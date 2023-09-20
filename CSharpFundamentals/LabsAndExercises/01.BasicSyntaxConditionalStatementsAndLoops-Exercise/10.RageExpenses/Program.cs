4037791eab0f98bd0a7bd1d1da4d1ed3fdb5fd9f
Martin <martin.geor04@gmail.com>
2023-09-20 23:37:39 +0300

    Completed 10

---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-Exercise/10.RageExpenses/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _10.RageExpenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int timesHeadsetDestroyed = lostGamesCount / 2;
+            int timesMouseDestroyed = lostGamesCount / 3;
+            int timesKeyboardDestroyed = lostGamesCount / 6;
+            int timesDisplayDestroyed = lostGamesCount / 12;
+
+            double headsetExpenses = headsetPrice * timesHeadsetDestroyed; 
+            double mouseExpenses = mousePrice * timesMouseDestroyed; 
+            double keyboardExpenses = keyboardPrice * timesKeyboardDestroyed; 
+            double displayExpenses = displayPrice * timesDisplayDestroyed;
+
+            double rageExpenses = headsetExpenses + mouseExpenses + keyboardExpenses + displayExpenses;
+            Console.WriteLine("Rage expenses: {0:f2} lv.", rageExpenses);
+        }
+    }
+}

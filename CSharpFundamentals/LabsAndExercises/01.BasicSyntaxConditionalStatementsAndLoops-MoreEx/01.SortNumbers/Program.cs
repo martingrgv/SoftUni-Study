52c6ccee5084b5098f5cc45bae5e76e1e9900c0f
Martin <martin.geor04@gmail.com>
2023-09-21 00:25:50 +0300

    Completed 01

---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-MoreEx/01.SortNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int[] numbers = { a, b, c };
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}

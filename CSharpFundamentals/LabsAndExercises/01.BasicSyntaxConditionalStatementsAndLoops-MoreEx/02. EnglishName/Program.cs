15273882f8e015a1fc3094a693bbdbd83919e0a0
Martin <martin.geor04@gmail.com>
2023-09-21 00:26:09 +0300

    Completed 02

---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-MoreEx/02. EnglishName/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02._EnglishName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int lastDigit = n % 10;
+            string digitName = String.Empty;
+
+            switch (lastDigit)
+            {
+                case 0:
+                    digitName = "zero";
+                    break;
+                case 1:
+                    digitName = "one";
+                    break;
+                case 2:
+                    digitName = "two";
+                    break;
+                case 3:
+                    digitName = "three";
+                    break;
+                case 4:
+                    digitName = "four";
+                    break;
+                case 5:
+                    digitName = "five";
+                    break;
+                case 6:
+                    digitName = "six";
+                    break;
+                case 7:
+                    digitName = "seven";
+                    break;
+                case 8:
+                    digitName = "eight";
+                    break;
+                case 9:
+                    digitName = "nine";
+                    break;
+            }
+            Console.WriteLine(digitName);
+        }
+    }
+}

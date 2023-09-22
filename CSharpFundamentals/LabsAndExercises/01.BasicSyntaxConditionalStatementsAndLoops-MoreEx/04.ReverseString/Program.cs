7b875236fe0beda67a649567c731a306732c0b2d
Martin <martin.geor04@gmail.com>
2023-09-22 22:25:12 +0300

    Completed 04

---
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-MoreEx/04.ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string sReverse = String.Empty;
+
+            for (int i = s.Length; i > 0; i--)
+            {
+                sReverse += s[i - 1];
+            }
+
+            Console.WriteLine(sReverse);
+        }
+    }
+}

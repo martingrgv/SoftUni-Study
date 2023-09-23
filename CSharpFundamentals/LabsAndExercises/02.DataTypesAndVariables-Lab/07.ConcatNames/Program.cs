c671b887036f5a4cf7ee2db215c3b17fb7277209
Martin <martin.geor04@gmail.com>
2023-09-23 15:05:13 +0300

    Completed DataTypes 07

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/07.ConcatNames/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.ConcatNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] concatName = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                concatName[i] = Console.ReadLine();   
+            }
+
+            Console.WriteLine(concatName[0] + concatName[2] + concatName[1]);
+        }
+    }
+}

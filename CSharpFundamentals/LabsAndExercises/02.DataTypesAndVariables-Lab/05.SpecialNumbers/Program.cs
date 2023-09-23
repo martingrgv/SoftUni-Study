459e2e7eaaa8db585d5365beeb45c16708c42576
Martin <martin.geor04@gmail.com>
2023-09-23 15:05:01 +0300

    Completed DataTypes 05

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i >= 10)
+                {
+                    int sumOfDigits = 0;
+                    int currentNumber = i;
+
+                    // Calculate sum of digits
+                    while (currentNumber > 0)
+                    {
+                        int lastDigit = currentNumber % 10;
+                        sumOfDigits += lastDigit;
+                        currentNumber = currentNumber / 10;
+                    }
+
+                    if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                    {
+                        Console.WriteLine("{0} -> True", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} -> False", i);
+                    }
+                }
+                else
+                {
+                    if (i == 5 || i == 7)
+                    {
+                        Console.WriteLine("{0} -> True", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} -> False", i);
+                    }
+                }
+            }
+        }
+    }
+}

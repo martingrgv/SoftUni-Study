6a3557fb795a2dd08fd7407dd6818bd310ddf2dc
Martin <martin.geor04@gmail.com>
2023-09-23 15:04:30 +0300

    Completed DataTypes 02

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/02.PoundsToDollars/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float pounds = float.Parse(Console.ReadLine());
+
+            float dollars = pounds * 1.31f;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}

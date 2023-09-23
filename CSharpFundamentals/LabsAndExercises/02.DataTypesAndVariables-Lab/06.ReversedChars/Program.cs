b29e7a1713f9b76f91964d00d2193ec533e8c275
Martin <martin.geor04@gmail.com>
2023-09-23 15:05:07 +0300

    Completed DataTypes 06

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/06.ReversedChars/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.ReversedChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] collectionOfChars = new char[3];
+            for (int i = 0; i < 3; i++) // Add chars to array
+            {
+                collectionOfChars[i] = char.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(collectionOfChars);
+            foreach (char letter in collectionOfChars) // Print reversed array
+            {
+                Console.Write(letter + " ");
+            }
+        }
+    }
+}

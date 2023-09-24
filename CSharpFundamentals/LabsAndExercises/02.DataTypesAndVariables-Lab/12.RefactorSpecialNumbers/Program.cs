7c11d02eafc50c316da115985904db094a205346
Martin <martin.geor04@gmail.com>
2023-09-25 02:30:46 +0300

    Completed DataTypes 12

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _12.RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            int n = int.Parse(Console.ReadLine());
+
+            // Logic
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int currentNum = i;
+
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                bool isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isSpecialNum);
+            }
+        }
+    }
+}

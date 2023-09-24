def6f603b8a0af9587d89fcf17b4d8a38bfe2efd
Martin <martin.geor04@gmail.com>
2023-09-25 02:29:45 +0300

    Completed 08

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/08.TownInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _08.TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+            // Output
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}

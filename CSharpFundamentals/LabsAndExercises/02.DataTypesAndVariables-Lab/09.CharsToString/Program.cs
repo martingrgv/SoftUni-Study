eb8a9b073e49f3239cf43b3a96d771cfc37d300b
Martin <martin.geor04@gmail.com>
2023-09-25 02:30:11 +0300

    Completed DataTypes 09

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/09.CharsToString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = String.Empty;
+
+            for (int i = 0; i < 3; i++)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                output += currentChar;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}

3e2fa95217d284584ef45f31a07b8ae1aaf1737f
Martin <martin.geor04@gmail.com>
2023-09-25 02:30:37 +0300

    Completed DataTypes 11

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/11.RefractorVolumeOfPyramid/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _11.RefractorVolumeOfPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+            Console.Write("Height: ");
+
+            double vol = (l * w * h) / 3;
+            Console.Write($"Pyramid Volume: {vol:f2}");
+        }
+    }
+}

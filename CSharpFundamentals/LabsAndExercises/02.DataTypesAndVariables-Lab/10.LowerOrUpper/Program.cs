0fb71e5366ac32b6e4b528fb9d0a2d58436f93f1
Martin <martin.geor04@gmail.com>
2023-09-25 02:30:27 +0300

    Completed DataTypes 10

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Lab/10.LowerOrUpper/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10.LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+            
+            if (Char.IsUpper(ch))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}

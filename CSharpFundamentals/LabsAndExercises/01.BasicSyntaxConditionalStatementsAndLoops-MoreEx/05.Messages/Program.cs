6a7c7ff59b2461abbccbf8faaf11ba39640d4b08
Martin <martin.geor04@gmail.com>
2023-09-25 21:49:33 +0300

    Completed BassicSynthaxMoreEx 05

--- CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-MoreEx/05.Messages/Program.cs
+++ CSharpFundamentals/LabsAndExercises/01.BasicSyntaxConditionalStatementsAndLoops-MoreEx/05.Messages/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace _05.Messages
+﻿using System;
+
+namespace _05.Messages
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(char(97));
+            char asciiValue;
+            string output = String.Empty;
+
+            // Input
+            byte timesRead = byte.Parse(Console.ReadLine());
+
+            // Logic
+            for (int i = 0; i < timesRead; i++)
+            {
+                ushort currentDigit = ushort.Parse(Console.ReadLine());
+
+                if (currentDigit == 0)
+                {
+                    asciiValue = AsciiToChar(32);
+                }
+                else
+                {
+                    byte mainDigit = (byte)(currentDigit % 10);
+                    byte digitLength = 0;
+                    while (currentDigit > 0)
+                    {
+                        digitLength++;
+                        currentDigit = (byte)(currentDigit / 10);
+                    }
+
+                    byte offset = (byte)((mainDigit - 2) * 3);
+                    if (mainDigit == 7 || mainDigit == 9)
+                    {
+                        offset++;
+                    }
+
+                    byte letterIndex = (byte)(offset + digitLength - 1);
+                    asciiValue = AsciiToChar((byte)(97 + letterIndex));
+                }
+            
+                output += asciiValue;
+            }
+
+            // Output
+            Console.WriteLine(output);
+        }
+        
+        private static char AsciiToChar(byte asciiIndex)
+        {
+            char ch = Convert.ToChar(asciiIndex);
+            return ch;
         }
     }
 }

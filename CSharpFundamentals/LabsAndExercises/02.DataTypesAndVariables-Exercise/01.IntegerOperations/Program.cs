7032c97e08cdc30fcf544977021e7a8a22a4eb98
Martin <martin.geor04@gmail.com>
2023-09-26 20:54:29 +0300

    Completed 02.DataTypes-Ex/01

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            int sum = firstNum + secondNum;
+            int divider = sum / thirdNum;
+            int multiplier = divider * fourthNum;
+
+            Console.WriteLine(multiplier);
+        }
+    }
+}

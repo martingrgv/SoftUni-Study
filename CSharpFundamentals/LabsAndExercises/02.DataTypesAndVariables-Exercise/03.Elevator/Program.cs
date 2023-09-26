8125fc1211749ec5e40a75eafd67b9cf64ee2967
Martin <martin.geor04@gmail.com>
2023-09-26 20:54:46 +0300

    Completed 02.DataTypes-Ex/03

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/03.Elevator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            byte peopleCount = byte.Parse(Console.ReadLine());
+            byte capacity = byte.Parse(Console.ReadLine());
+
+            // Logic
+            float elevatorCourses = (float)(Math.Ceiling((double)peopleCount / capacity));
+
+            // Output
+            Console.WriteLine(elevatorCourses);
+        }
+    }
+}

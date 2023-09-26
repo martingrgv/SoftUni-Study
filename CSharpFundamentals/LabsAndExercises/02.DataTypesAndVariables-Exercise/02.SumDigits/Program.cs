6d28ef88eddd320976973d6b90c5021abc7d67e6
Martin <martin.geor04@gmail.com>
2023-09-26 20:54:41 +0300

    Completed 02.DataTypes-Ex/02

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/02.SumDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            // Input
+            int number = int.Parse(Console.ReadLine());
+
+            // Logic
+            int currentNumber = number;
+            while (currentNumber > 0)
+            {
+                sum += currentNumber % 10;
+                currentNumber /= 10;
+            }
+            // Output
+            Console.WriteLine(sum);
+        }
+    }
+}

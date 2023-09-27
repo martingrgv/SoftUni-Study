2dab1c995e388e813094a513440df487a01a357e
Martin <martin.geor04@gmail.com>
2023-09-27 23:15:52 +0300

    Completed DataTypes-Ex 07

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte tankCapacity = 255;
+            short litresPoured = 0;
+            byte readTimes = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < readTimes; i++)
+            {
+                short currentLitres = short.Parse(Console.ReadLine());
+
+                if (currentLitres <= tankCapacity - litresPoured)
+                {
+                    litresPoured += currentLitres;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(litresPoured);
+        }
+    }
+}

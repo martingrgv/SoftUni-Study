f1c2f3075beb776f45cfafa79a8b947213660c35
Martin <martin.geor04@gmail.com>
2023-09-27 23:15:57 +0300

    Completed DataTypes-Ex 08

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kegs = int.Parse(Console.ReadLine());
+
+            string biggestKegName = String.Empty;
+            double biggestKegArea = 0;
+
+            for (int i = 0; i < kegs; i++)
+            {
+                string currentKegName = String.Empty;
+                double currentKegArea = 0;
+
+                // Get keg info
+                currentKegName = Console.ReadLine();
+
+                double radius = double.Parse(Console.ReadLine());
+                long height = long.Parse(Console.ReadLine());
+
+                currentKegArea = Math.PI * Math.Pow(radius, 2) * height;
+
+                // Find biggest keg
+                if (biggestKegArea < currentKegArea)
+                {
+                    biggestKegName = currentKegName;
+                    biggestKegArea = currentKegArea;
+                }
+            }
+            Console.WriteLine(biggestKegName);
+        }
+    }
+}

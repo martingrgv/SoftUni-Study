834b1d267c5e00c0bff1805f19107f3b13ab3500
Martin <martin.geor04@gmail.com>
2023-09-27 23:15:38 +0300

    Completed DataTypes-Ex 05

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                char asciiValue = ConvertToASCII(i);
+                Console.Write(asciiValue + " ");
+            }
+        }
+
+        static char ConvertToASCII(int number)
+        {
+            return Convert.ToChar(number);
+        }
+    }
+}

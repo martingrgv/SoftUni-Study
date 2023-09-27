a5554c12def04ece01f508f858df1b27b2b9b4a2
Martin <martin.geor04@gmail.com>
2023-09-27 23:15:45 +0300

    Completed DataTypes-Ex 06

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int finishNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < finishNum; i++)
+            {
+                char firstChar = (char)('a' + i);
+                for (int j = 0; j < finishNum; j++)
+                {
+                    char secondChar = (char)('a' + j);
+                    for (int k = 0; k < finishNum; k++)
+                    {
+                        char thirdChar = (char)('a' + k);
+
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}

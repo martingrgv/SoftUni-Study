fc0d7f5c45a0f0502a1e9e6ba5c3a887b047ef5c
Martin <martin.geor04@gmail.com>
2023-09-30 02:39:39 +0300

    Completed Arrays-Lab 04

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ").ToArray();
+
+            foreach (var element in arr.Reverse())
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}

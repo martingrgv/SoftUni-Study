87e924e821804afdb0b259ff438cb80598ab2b5c
Martin <martin.geor04@gmail.com>
2023-09-30 02:39:31 +0300

    Completed Arrays-Lab 03

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numbers = Console.ReadLine()
+                                     .Split(" ")
+                                     .Select(float.Parse)
+                                     .ToArray();
+            int[] roundedNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < roundedNumbers.Length; i++)
+            {
+                roundedNumbers[i] = (int) Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine(numbers[i] + " => " + roundedNumbers[i]);
+            }
+        }
+    }
+}

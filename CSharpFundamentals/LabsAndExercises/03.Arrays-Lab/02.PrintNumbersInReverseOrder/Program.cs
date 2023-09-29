22397d91513512f727bc102ada7de0c02cd032e0
Martin <martin.geor04@gmail.com>
2023-09-30 02:39:23 +0300

    Completed Arrays-Lab 02

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.PrintNumbersInReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            // Read numbers
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Print array reversed
+            for (int i = numbers.Length; i > 0; i--)
+            {
+                Console.Write(numbers[i - 1] + " ");
+            }
+        }
+    }
+}

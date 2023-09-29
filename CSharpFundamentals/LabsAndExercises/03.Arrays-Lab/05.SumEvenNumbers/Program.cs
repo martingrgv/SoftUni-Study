73bbdc2b19baad5d7d4e9867e514cd69d8243ca8
Martin <martin.geor04@gmail.com>
2023-09-30 02:39:50 +0300

    Completed Arrays-Lab 05

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05.SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            var sumOfEvens = 0;
+
+            foreach (var num in evenNumbers)
+            {
+                sumOfEvens += num;
+            }
+            Console.WriteLine(sumOfEvens);
+        }
+    }
+}

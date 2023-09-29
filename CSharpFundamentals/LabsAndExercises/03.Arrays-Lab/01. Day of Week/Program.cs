0d9a63ccfd4c605bb561e51a09c20efbb4fbd1eb
Martin <martin.geor04@gmail.com>
2023-09-30 02:39:08 +0300

    Completed Arrays-Lab 01

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Lab/01. Day of Week/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int day = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(weekdays[day - 1]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}

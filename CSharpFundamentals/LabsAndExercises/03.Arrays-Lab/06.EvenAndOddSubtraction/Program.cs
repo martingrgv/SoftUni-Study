8aec00bde88a6dd19338cd415e67cb719b25a6bb
Martin <martin.geor04@gmail.com>
2023-09-30 02:45:15 +0300

    Completed Arrays-Lab 06

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Lab/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToArray();
+            var evenNumbers = numbers.Where(n => n % 2 == 0).ToArray();
+            var oddNumbers = numbers.Where(n => n % 2 != 0).ToArray();
+
+            int evenNumbersSum = GetElemetsSum(evenNumbers);
+            int oddNumbersSum = GetElemetsSum(oddNumbers);
+            int diff = evenNumbersSum - oddNumbersSum;
+
+            Console.WriteLine(diff);
+        }
+
+        static int GetElemetsSum(int[] arr)
+        {
+            int sum = 0;
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}

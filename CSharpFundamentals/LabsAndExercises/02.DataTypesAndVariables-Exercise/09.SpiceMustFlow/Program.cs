1e9ca3d36eda61a85b12a15b520b26b55d3e6a6f
Martin <martin.geor04@gmail.com>
2023-10-01 18:27:31 +0300

    Completed DataTypes-Ex 09

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yieldSource = int.Parse(Console.ReadLine());
+            int yieldStored = 0;
+            int daysOperated = 0;
+
+            while (yieldSource >= 100)
+            {
+                yieldStored += yieldSource - 26;
+
+                daysOperated++;
+                yieldSource -= 10;
+            }
+            if (yieldStored > 26)
+            {
+                yieldStored -= 26;
+            }
+
+            Console.WriteLine(daysOperated);
+            Console.WriteLine(yieldStored);
+        }
+    }
+}

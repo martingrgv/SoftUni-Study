3209fd84d718f23d646010f435dcfcd4c9cdcb52
Martin <martin.geor04@gmail.com>
2023-10-02 10:49:44 +0300

    Completed DataTypes-Ex 11

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/11.Snowballs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace _11.Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballs = int.Parse(Console.ReadLine());
+
+            int highestSnowballSnow = 0;
+            int highestSnowballTime = 0;
+            int highestSnowballQuality = 0;
+            BigInteger highestSnowballValue = 0;
+
+            for (int i = 0; i < snowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+                if (snowballValue > highestSnowballValue)
+                {
+                    highestSnowballSnow = snowballSnow;
+                    highestSnowballTime = snowballTime;
+                    highestSnowballQuality = snowballQuality;
+                    highestSnowballValue = snowballValue;
+                }
+            }
+
+            Console.WriteLine($"{highestSnowballSnow} : {highestSnowballTime} = {highestSnowballValue} ({highestSnowballQuality})");
+        }
+    }
+}

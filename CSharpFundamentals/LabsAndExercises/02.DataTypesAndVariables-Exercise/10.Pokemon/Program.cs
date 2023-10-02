b9e11a1b68df6f004f0ad9ca50c6c956055d0080
Martin <martin.geor04@gmail.com>
2023-10-02 10:49:33 +0300

    c

---
+++ CSharpFundamentals/LabsAndExercises/02.DataTypesAndVariables-Exercise/10.Pokemon/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _10.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int currentPower = power; // N
+            int distance = int.Parse(Console.ReadLine()); // M
+            int exhaustionFactor = int.Parse(Console.ReadLine()); // Y
+            int targetsPoked = 0;
+
+            while (currentPower >= distance)
+            {
+                currentPower -= distance;
+                targetsPoked++;
+
+                if (power / 2 == currentPower && exhaustionFactor != 0)
+                {
+                    currentPower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(currentPower);
+            Console.WriteLine(targetsPoked);
+        }
+    }
+}

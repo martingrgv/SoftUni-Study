54e742cf5a8a71ab6ab9f7b2e248f7187a99fc92
Martin <martin.geor04@gmail.com>
2023-10-05 19:33:10 +0300

    Completed Arrays-Ex 05

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Exercise/05.TopIntegers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        break;
+                    }
+                    if (j == numbers.Length - 1)
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+            }
+            Console.WriteLine(numbers[^1]);
+        }
+    }
+}

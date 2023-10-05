5254caa99d3bf5e01ec15d2fe5c6bdf1e6726561
Martin <martin.geor04@gmail.com>
2023-10-05 19:32:46 +0300

    Completed Arrays-Ex 02

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Exercise/02.CommonElements/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine().Split(" ").ToArray();
+            string[] secondArr = Console.ReadLine().Split(" ").ToArray();
+
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                for (int j = 0; j < firstArr.Length; j++)
+                {
+                    if (secondArr[i] == firstArr[j])
+                    {
+                        Console.Write(secondArr[i] + " ");
+                    }
+                }
+            }
+        }   
+    }
+}

2002d60d6b9a6940c662474708c6a8aa1a58d0ac
Martin <martin.geor04@gmail.com>
2023-10-05 19:32:50 +0300

    Completed Arrays-Ex 03

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            // Write to array
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentArr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                if (i % 2 == 0) // Is not even
+                {
+                    arr1[i] = currentArr[0];
+                    arr2[i] = currentArr[1];
+                }
+                else // Is even
+                {
+                    arr2[i] = currentArr[0];
+                    arr1[i] = currentArr[1];
+                }
+            }
+
+            // Read array
+            foreach (int num in arr1)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            foreach (int num in arr2)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}

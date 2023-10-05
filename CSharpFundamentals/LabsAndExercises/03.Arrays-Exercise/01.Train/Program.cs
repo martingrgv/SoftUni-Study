9f5472c7d3ef695a3418add430caaaa54695b9ff
Martin <martin.geor04@gmail.com>
2023-10-05 19:32:39 +0300

    Completed Arrays-Ex 01

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Exercise/01.Train/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonsCount = int.Parse(Console.ReadLine());
+            int[] wagons = new int[wagonsCount];
+
+            // Write to array
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Read from array
+            int totalPassengers = 0;
+
+            foreach (int passengers in wagons)
+            {
+                Console.Write(passengers + " ");
+                totalPassengers += passengers;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}

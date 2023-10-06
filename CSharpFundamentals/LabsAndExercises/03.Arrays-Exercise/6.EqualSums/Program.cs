37fdd4170d0b051be33dfe0975d19b065bb91c22
Martin <martin.geor04@gmail.com>
2023-10-07 02:33:36 +0300

    Completed Arrays-Ex 06

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Exercise/6.EqualSums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace _6.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                rightSum += numbers[i];
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                rightSum -= numbers[i];
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+                else
+                {
+                    leftSum += numbers[i];
+                }
+            }
+
+            Console.WriteLine("no");
+        }
+    }
+}

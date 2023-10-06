f9b3a107878d440325de69149a98dbaadcbb76a9
Martin <martin.geor04@gmail.com>
2023-10-07 02:33:43 +0300

    Completed Arrays-Ex 06

---
+++ CSharpFundamentals/LabsAndExercises/03.Arrays-Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Tracing;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int longestSequenceCount = 0;
+            int longestSequenceNum = 0;
+
+            int sequenceCount = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    sequenceCount++;
+                    
+                    if (sequenceCount > longestSequenceCount)
+                    {
+                        longestSequenceCount = sequenceCount;
+                        longestSequenceNum = numbers[i];
+                    }
+                }
+                else
+                {
+                    sequenceCount = 0;
+                }
+            }
+            
+            for (int i = 0; i <= longestSequenceCount; i++)
+            {
+                Console.Write(longestSequenceNum + " ");
+            }
+        }
+    }
+}

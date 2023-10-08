9d004f092e375db7e22f3ae1b804a25fce7b8f97
Martin <martin.geor04@gmail.com>
2023-10-08 16:05:25 +0300

    Completed Methods-Lab 05

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/05.Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Transactions;
+
+namespace _05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int amount = int.Parse(Console.ReadLine());
+
+            double price = GetPrice(product, amount);
+            double total = GetTotalPrice(price, amount);
+
+            Console.WriteLine($"{total:f2}");
+        }
+
+        static double GetPrice(string product, int amount = 1)
+        {
+            switch (product)
+            {
+                case "coffee":
+                    return 1.5;
+                case "water":
+                    return 1;
+                case "coke":
+                    return 1.4;
+                case "snacks":
+                    return 2;
+            }
+            return 0;
+        }
+
+        static double GetTotalPrice(double pricePerProduct, int amount = 1)
+        {
+            return pricePerProduct * amount;
+        }
+    }
+}

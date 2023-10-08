a411c8c4001497674b157dbb2628ee9463806901
Martin <martin.geor04@gmail.com>
2023-10-08 16:05:04 +0300

    Completed Methods-Lab 01

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string state = ChechNumberState(n);
+
+            Console.WriteLine("The number {0} is {1}.", n, state);
+        }
+
+        static string ChechNumberState(int n)
+        {
+            if (n > 0)
+            {
+                return "positive";
+            }
+            else if (n == 0)
+            {
+                return "zero";
+            }
+            else
+            {
+                return "negative";
+            }
+        }
+    }
+}

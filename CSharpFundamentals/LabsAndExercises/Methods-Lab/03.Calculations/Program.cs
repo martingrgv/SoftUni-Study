6d0430ab63123e278a27cde73909f7eb3ad5f2a8
Martin <martin.geor04@gmail.com>
2023-10-08 16:05:15 +0300

    Completed Methods-Lab 03

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/03.Calculations/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int fN = int.Parse(Console.ReadLine());
+            int lN = int.Parse(Console.ReadLine());
+
+            Calculate(fN, lN, operation);
+        }
+
+        static void Calculate(int firstNumber, int lastNumber, string operation)
+        {
+            if (operation == "add")
+            {
+                Console.WriteLine(firstNumber + lastNumber);
+            }
+            else if (operation == "subtract")
+            {
+                Console.WriteLine(firstNumber - lastNumber);
+            }
+            else if (operation == "multiply")
+            {
+                Console.WriteLine(firstNumber * lastNumber);
+            }
+            else
+            {
+                Console.WriteLine(firstNumber / lastNumber);
+            }
+        }
+    }
+}

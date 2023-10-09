a869c55673de6dad8f6fc93b554302d9caeed658
Martin <martin.geor04@gmail.com>
2023-10-09 14:38:48 +0300

    Completed Methods-Lab 08

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/08.MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double answ = Pow(@base, power);
+
+            Console.WriteLine(answ);
+        }
+
+        static double Pow(double number, double power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}

af818a8dc91bea1d23d2ff580ef7a7b65ab1a76f
Martin <martin.geor04@gmail.com>
2023-10-09 14:39:06 +0300

    Completed Methods-Lab 11

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/11.MathOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double lastNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(firstNum, @operator, lastNum));
+        }
+
+        static double Calculate(double a, char @operator, double b)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}

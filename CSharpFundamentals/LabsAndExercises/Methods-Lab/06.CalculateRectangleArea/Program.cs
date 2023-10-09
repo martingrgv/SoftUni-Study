ffc24acaf6620421863af77cda4f6fde36f9fcc1
Martin <martin.geor04@gmail.com>
2023-10-09 14:38:34 +0300

    Completed Methods-Lab 06

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double area = GetRectangleArea(a, b);
+
+            Console.WriteLine(area);
+        }
+
+        static double GetRectangleArea(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}

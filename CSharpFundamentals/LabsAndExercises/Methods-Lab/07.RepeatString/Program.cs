a54d3134c3e563a62e633e989f2a084ab0083084
Martin <martin.geor04@gmail.com>
2023-10-09 14:38:42 +0300

    Completed Methods-Lab 07

---
+++ CSharpFundamentals/LabsAndExercises/Methods-Lab/07.RepeatString/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            text = GetRepeatedString(text, n);
+
+            Console.WriteLine(text);
+        }
+
+        static string GetRepeatedString(string s, int n)
+        {
+            string result = String.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                result += s;
+            }
+
+            return result;
+        }
+    }
+}

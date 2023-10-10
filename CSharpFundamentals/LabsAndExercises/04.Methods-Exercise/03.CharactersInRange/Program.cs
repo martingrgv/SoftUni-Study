eb18e0fed02e5a6dad35bf0d4802a8a48c4a3109
Martin <martin.geor04@gmail.com>
2023-10-10 14:48:03 +0300

    Completed 04.Methods-Ex 03

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.CharactersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCh = char.Parse(Console.ReadLine());
+            char lastCh = char.Parse(Console.ReadLine());
+
+            if (firstCh < lastCh)
+                PrintASCII(firstCh, lastCh);
+            else
+                PrintASCII(lastCh, firstCh);
+        }
+
+        static void PrintASCII(char start, char end)
+        {
+            char currentChar = (char)(start + 1);
+            while (currentChar != end)
+            {
+                Console.Write(currentChar++ + " ");
+            }
+        }
+    }
+}

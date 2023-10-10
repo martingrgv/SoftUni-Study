66865dd658b1a3bd4886e805a45eb92da49f064c
Martin <martin.geor04@gmail.com>
2023-10-10 13:50:22 +0300

    Completed 04.Methods-Ex 01

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.SmallestOfThreeNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[3];
+        ReadInput(numbers);
+
+        int lowestNum = GetLowestInt(numbers);
+        Console.WriteLine(lowestNum);
+    }
+
+    static void ReadInput(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    static int GetLowestInt(int[] numbers)
+    {
+        int lowestNum = numbers[0];
+        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < lowestNum)
+            {
+                lowestNum = numbers[i];
+            }
+        }
+
+        return lowestNum;
+    }
+}

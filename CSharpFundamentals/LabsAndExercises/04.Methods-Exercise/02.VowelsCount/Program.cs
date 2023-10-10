81ebd30dc62f0a1423a3680d52236424b8c6befa
Martin <martin.geor04@gmail.com>
2023-10-10 14:37:49 +0300

    Completed 04.Methods-Ex 02

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.VowelsCount;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        PrintVowels(word);
+    }
+
+    static int GetVowelsCount(string word)
+    {
+        int total = 0;
+        foreach (char letter in word)
+        {
+            if (isVowel(letter))
+            {
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    static bool isVowel(char letter) => Char.ToLower(letter) == 'a' || Char.ToLower(letter) == 'e' || 
+                                        Char.ToLower(letter) == 'i'  || Char.ToLower(letter)== 'o'  || 
+                                        Char.ToLower(letter) == 'u'
+                                        ? true : false;
+
+    static void PrintVowels(string word)
+    {
+        int vowelsCount = GetVowelsCount(word);
+        Console.WriteLine(vowelsCount);
+    }
+}

0a0892df192c20ed878f645b0fd13fb028c45fed
Martin <martin.geor04@gmail.com>
2023-10-11 11:44:54 +0300

    Completed 04.Methods-Ex 08

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+
+namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fNum = int.Parse(Console.ReadLine());
+            int lNum = int.Parse(Console.ReadLine());
+
+            double fFactorial = GetFactorial(fNum);
+            double lFactorial = GetFactorial(lNum);
+
+            double result = fFactorial / lFactorial;
+
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static double GetFactorial(double num)
+        {
+            double factorial = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+    }
+}

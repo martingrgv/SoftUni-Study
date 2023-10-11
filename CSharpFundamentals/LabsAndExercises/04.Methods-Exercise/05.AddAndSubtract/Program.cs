17ba07e861b73195931b913c3b732510838cf0e5
Martin <martin.geor04@gmail.com>
2023-10-11 10:33:00 +0300

    Completed 04.Methods-Ex 05

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace _05.AddAndSubtract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = GetNumbers();
+
+        int sum = AddFirstNumbers(numbers[0], numbers[1]);
+        int result = SubtractLastNumbers(sum, numbers[2]);
+
+        Console.WriteLine(result);
+    }
+
+    static int[] GetNumbers()
+    {
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        return numbers;
+    }
+
+    static int AddFirstNumbers(int fNum, int sNum) => fNum + sNum;
+
+    static int SubtractLastNumbers(int sum, int lNum) => sum - lNum;
+}

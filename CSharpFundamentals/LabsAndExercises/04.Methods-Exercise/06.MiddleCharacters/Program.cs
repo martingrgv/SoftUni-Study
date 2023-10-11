946698fede29ab6d63aa5bd5f21607b51042b4dd
Martin <martin.geor04@gmail.com>
2023-10-11 10:48:05 +0300

    Completed 04.Methods-Ex 06

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] middleChars = GetMiddleChar(input);
+
+            foreach (char ch in middleChars)
+            {
+                Console.Write(ch);
+            }
+        }
+
+        static char[] GetMiddleChar(string s)
+        {
+            if (s.Length % 2 == 0) // Is even
+            {
+                char[] arr = new char[2];
+
+                arr[0] = s[s.Length / 2 - 1];
+                arr[1] = s[s.Length / 2];
+
+                return arr;
+            }
+            else // Is odd
+            {
+                char[] arr = new char[1];
+
+                arr[0] = s[s.Length / 2];
+
+                return arr;
+            }
+        }
+    }
+}

039f073b52d96e5cb6cb82c821a519605020f731
Martin <martin.geor04@gmail.com>
2023-10-11 11:44:45 +0300

    Completed 04.Methods-Ex 07

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/07.NxNMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            PrintMatrix(matrix, n);
+        }
+
+        static void PrintMatrix(int[,] matrix, int n)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = n;
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

1d07a7a803514224b277dcb74f22fabd72ff6789
Martin <martin.geor04@gmail.com>
2023-10-12 04:07:33 +0300

    Completed 04.Methods-Ex 09

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = GetNumbers();
+            
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(isPalindrome(num).ToString().ToLower());
+            }
+        }
+
+        static List<int> GetNumbers()
+        {
+            string userInput;
+            List<int> list = new List<int>();
+
+            while ((userInput = Console.ReadLine()) != "END")
+            {
+                list.Add(int.Parse(userInput));
+            }
+
+            return list;
+        }
+
+        static bool isPalindrome(int num)
+        {
+            int before = num;
+            int after = 0;
+
+            while (num > 0)
+            {
+                after = after * 10 + num % 10;
+                num = num / 10;
+            }
+
+            return before == after;
+        }
+    }
+}

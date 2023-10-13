d1ce959cd7309e9a2f146e360f71df113574bb1c
Martin <martin.geor04@gmail.com>
2023-10-13 21:20:50 +0300

    Completed 04.Methods-Ex 10

---
+++ CSharpFundamentals/LabsAndExercises/04.Methods-Exercise/10.TopNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+
+namespace _10.TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int endValue = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= endValue; i++)
+            {
+                if (isSumDivisible(i) && holdsOddDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool isSumDivisible(int num)
+        {
+            int sum = 0;
+            
+            while (num > 0)
+            {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+
+            if (sum % 8 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool holdsOddDigit(int num)
+        {
+            while (num > 0)
+            {
+                if ((num % 10) % 2 != 0)
+                {
+                    return true;
+                }
+                num = num / 10;
+            }
+
+            return false;
+        }
+    }
+}

7ce5a21ebd1cc66fe0b0f199dc6b94ee5ef58164
Martin <martin.geor04@gmail.com>
2023-10-16 15:18:21 +0300

    Completed 05.Lists-Lab 04

---
+++ CSharpFundamentals/LabsAndExercises/05.Lists-Lab/4.ListOfProducts/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _4.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int productCount = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 1; i <= productCount; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+
+            for (int i = 1; i <= products.Count; i++)
+            {
+                Console.WriteLine($"{i}.{products[i - 1]}");
+            }
+        }
+    }
+}

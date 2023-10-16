acf4e986873da3ebb9bd3d0d989ac254125c9546
Martin <martin.geor04@gmail.com>
2023-10-16 15:18:10 +0300

    Completed 05.Lists-Lab 02

---
+++ CSharpFundamentals/LabsAndExercises/05.Lists-Lab/2.GaussTrick/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _2.GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Order first + last, first + 1 + last - 1, first + 2 + last - 2, … first + n, last - n.
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+
+            for (int i = 0, j = numbers.Count - 1; i < numbers.Count / 2; i++, j--)
+            {
+                result.Add(numbers[i] + numbers[j]);
+            }
+
+            if (numbers.Count % 2 != 0)
+            {
+                result.Add(numbers[numbers.Count / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}

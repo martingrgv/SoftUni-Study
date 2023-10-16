cbae3ee1b06374e15d6226192180fe474b5b997b
Martin <martin.geor04@gmail.com>
2023-10-16 15:18:31 +0300

    Completed 05.Lists-Lab 06

---
+++ CSharpFundamentals/LabsAndExercises/05.Lists-Lab/6.ListManipulationBasics/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _6.ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string[] userInput = Console.ReadLine().Split(" ");
+
+            while (userInput[0] != "end")
+            {
+                if (userInput[0] == "Add")
+                {
+                    int number = int.Parse(userInput[1]);
+                    numbers.Add(number);
+                }
+                else if (userInput[0] == "Remove")
+                {
+                    int number = int.Parse(userInput[1]);
+                    numbers.Remove(number);
+                }
+                else if (userInput[0] == "RemoveAt")
+                {
+                    int index = int.Parse(userInput[1]);
+                    numbers.RemoveAt(index);
+                }
+                else if (userInput[0] == "Insert")
+                {
+                    int number = int.Parse(userInput[1]);
+                    int index = int.Parse(userInput[2]);
+                    numbers.Insert(index, number);
+                }
+
+
+                userInput = Console.ReadLine().Split(" ");
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

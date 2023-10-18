fb046ece8ca0758bde35ccb895798cffe525affe
Martin <martin.geor04@gmail.com>
2023-10-18 20:44:52 +0300

    Completed 05.Lists-Ex 02

---
+++ CSharpFundamentals/LabsAndExercises/05.Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split(" ");
+
+            while (command[0] != "end")
+            {
+                if (command[0] == "Delete")
+                {
+                    int number = int.Parse(command[1]);
+                    Delete(numbers, number);
+                }
+                else // Insert
+                {
+                    int number = int.Parse(command[1]);
+                    int index = int.Parse(command[2]);
+                    numbers.Insert(index, number);
+                }
+
+                command = Console.ReadLine().Split(" ");
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static void Delete(List<int> numbers, int number)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    numbers.RemoveAt(i);
+                }
+            }
+        }
+    }
+}

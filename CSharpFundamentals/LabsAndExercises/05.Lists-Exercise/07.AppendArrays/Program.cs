7ec897aa89beb106c5567163114ab5382d490e16
Martin <martin.geor04@gmail.com>
2023-10-19 16:31:22 +0300

    Completed 05.Lists-Ex 07

---
+++ CSharpFundamentals/LabsAndExercises/05.Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrList = new List<int>();
+            string[] arrOfArrays = Console.ReadLine()
+                                       .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                                       .ToArray();
+
+            for (int i = arrOfArrays.Length - 1; i >= 0; i--)
+            {
+                int[] currentArr = arrOfArrays[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                foreach (int number in currentArr)
+                {
+                    arrList.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', arrList));
+        }
+    }
+}

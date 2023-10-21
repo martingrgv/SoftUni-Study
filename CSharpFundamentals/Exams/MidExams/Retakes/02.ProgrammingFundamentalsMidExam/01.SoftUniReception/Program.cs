05e67861cc08c08bc65581b3b137989e48411120
Martin <martin.geor04@gmail.com>
2023-10-21 23:33:15 +0300

    Completed 02 01

--- CSharpFundamentals/Exams/MidExams/Retakes/02.ProgrammingFundamentalsMidExam/01.SoftUniReception/Program.cs
+++ CSharpFundamentals/Exams/MidExams/Retakes/02.ProgrammingFundamentalsMidExam/01.SoftUniReception/Program.cs
@@ -10,29 +10,18 @@
         int studentsCount = int.Parse(Console.ReadLine());
         int hours = 0;
 
-       AnswerQuestions(maxCapacity, studentsCount, hours);
-
-    }
-
-    private static void AnswerQuestions(int maxCapacity, int studentsCount, int hours)
-    {
-        int studentsLeft = studentsCount - maxCapacity;
-
-        // Apply break
-        if (hours % 4 == 0)
+        while (studentsCount > 0)
         {
             hours++;
-        }
 
-        if (studentsLeft > 0)
-        {
-            hours++;
+            if (hours % 4 == 0)
+            {
+                hours++;
+            }
+
             studentsCount -= maxCapacity;
-            AnswerQuestions(maxCapacity, studentsCount, hours);
         }
-        else
-        {
-            System.Console.WriteLine($"Time needed: {hours}h.");
-        }
+
+        Console.WriteLine($"Time needed: {hours}h.");
     }
 }

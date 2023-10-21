25c4ee95d67548de78e471e0f65ecb4e2d58af39
Martin <martin.geor04@gmail.com>
2023-10-21 23:33:31 +0300

    Completed 02 03

--- CSharpFundamentals/Exams/MidExams/Retakes/02.ProgrammingFundamentalsMidExam/03.Numbers/Program.cs
+++ CSharpFundamentals/Exams/MidExams/Retakes/02.ProgrammingFundamentalsMidExam/03.Numbers/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<int> numbers = Console.ReadLine()
+                            .Split(" ")
+                            .Select(int.Parse)
+                            .ToList();
+
+        numbers.Sort();
+        numbers.Reverse();
+
+        double average = 0;
+
+        // Find average
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            average += numbers[i];
+        }
+
+        average = average / numbers.Count;
+
+        int printCount = 0;
+        bool foundAverage = false;
+
+        // Find greater
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > average && printCount < 5)
+            {
+                foundAverage = true;
+                printCount++;
+                
+                Console.Write($"{numbers[i]} ");
+            }
+            else
+            {
+                if (i == numbers.Count - 1 && !foundAverage)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+        }
     }
 }

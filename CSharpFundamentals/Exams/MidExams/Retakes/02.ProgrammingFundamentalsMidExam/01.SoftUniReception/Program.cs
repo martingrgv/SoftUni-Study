a63dbb11c4e38c6d87f8bce9213047a0a5d7ceb0
martingrgv <martin.geor04@gmail.com>
2023-10-21 18:02:00 +0300

    Added project files

---
+++ CSharpFundamentals/Exams/MidExams/Retakes/02.ProgrammingFundamentalsMidExam/01.SoftUniReception/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        int employeeMaxCapacity1 = int.Parse(Console.ReadLine());
+        int employeeMaxCapacity2 = int.Parse(Console.ReadLine());
+        int employeeMaxCapacity3 = int.Parse(Console.ReadLine());
+
+        int maxCapacity = employeeMaxCapacity1 + employeeMaxCapacity2 + employeeMaxCapacity3;
+        int studentsCount = int.Parse(Console.ReadLine());
+        int hours = 0;
+
+       AnswerQuestions(maxCapacity, studentsCount, hours);
+
+    }
+
+    private static void AnswerQuestions(int maxCapacity, int studentsCount, int hours)
+    {
+        int studentsLeft = studentsCount - maxCapacity;
+
+        // Apply break
+        if (hours % 4 == 0)
+        {
+            hours++;
+        }
+
+        if (studentsLeft > 0)
+        {
+            hours++;
+            studentsCount -= maxCapacity;
+            AnswerQuestions(maxCapacity, studentsCount, hours);
+        }
+        else
+        {
+            System.Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
---
+++ CSharpFundamentals/Exams/MidExams/Retakes/02.ProgrammingFundamentalsMidExam/03.Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _03.Numbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

c86b6d1bc3de9d6bc595be9647ff2ffc96fc7540
martingrgv <martin.geor04@gmail.com>
2023-10-21 06:04:33 +0300

    Completed 01 02

--- CSharpFundamentals/Exams/MidExams/02.TheLift/Program.cs
+++ CSharpFundamentals/Exams/MidExams/02.TheLift/Program.cs
@@ -4,7 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name = System.Console.ReadLine();
-        Console.WriteLine("Hello, World!");
+        // Wagon capacity = 4
+        int queue = int.Parse(Console.ReadLine());
+        int[] lifts = Console.ReadLine()
+                      .Split(" ")
+                      .Select(int.Parse)
+                      .ToArray();
+
+        bool hasEmptySpaces = true;
+
+        for (int i = 0; i < lifts.Length; i++)
+        {
+            while (lifts[i] < 4)
+            {
+                if (queue > 0)
+                {
+                    lifts[i]++;
+                    queue--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (lifts[i] < 4)
+            {
+                hasEmptySpaces = true;
+            }
+            else
+            {
+                hasEmptySpaces = false;
+            }
+        }
+
+        if (queue <= 0 && hasEmptySpaces)
+        {
+            System.Console.WriteLine("The lift has empty spots!");
+            System.Console.WriteLine(String.Join(" ", lifts));
+        }
+        else if (queue > 0 && !hasEmptySpaces)
+        {
+            System.Console.WriteLine($"There isn't enough space! {queue} people in a queue!");
+            System.Console.WriteLine(String.Join(" ", lifts));
+        }
+        else if (queue <= 0 && !hasEmptySpaces)
+        {
+            System.Console.WriteLine(String.Join(" ", lifts));
+        }
     }
 }

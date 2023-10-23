c93ea67d184b7cb96457cc8aa5fa9ba578d0ddd5
Martin <martin.geor04@gmail.com>
2023-10-23 16:07:52 +0300

    Completed 06.OOP 04.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/04.Students/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04.Students;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>();
+        string[] input = Console.ReadLine().Split(" ");
+
+        AddStudents(students, input);
+
+        string city = Console.ReadLine();
+
+        List<Student> homeTownStudents = students.Where(s => s.HomeTown == city).ToList();
+
+        foreach (var student in homeTownStudents)
+        {
+            System.Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+        }
+    }
+
+    public static void AddStudents(List<Student> students, string[] input)
+    {
+        while (input[0] != "end")
+        {
+            Student student = new Student(
+                input[0],
+                input[1],
+                int.Parse(input[2]),
+                input[3]);
+
+            students.Add(student);
+
+            input = Console.ReadLine().Split(" ");
+        }
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/04.Students/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Students;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string HomeTown { get; set; }
+
+    public Student(string firstName, string lastName, int age, string homeTown)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.HomeTown = homeTown;
+    }
+}

423209e8743d8021bda6eb57e5341209b1e99e8e
Martin <martin.geor04@gmail.com>
2023-10-23 15:42:15 +0300

    Completed 06.OOP 01.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                                .Split(" ");
+            Random rnd = new();
+            
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(0, words.Length);
+                string temp = words[pos1];
+
+                words[pos1] = words[pos2];
+                words[pos2] = temp;
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, words));
+        }
+    }
+}

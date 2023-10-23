42818ebdab3d4049cfcc33a3caddc5c68fe8cde0
Martin <martin.geor04@gmail.com>
2023-10-23 16:07:38 +0300

    Completed 06.OOP 03.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/03.Songs/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.Songs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int songsCount = int.Parse(Console.ReadLine());
+        List<Song> songs = new List<Song>();
+
+        for (int i = 0; i < songsCount; i++)
+        {
+            string[] fullName = Console.ReadLine().Split("_");
+
+            Song song = new Song();
+            song.TypeList = fullName[0];
+            song.Name = fullName[1];
+            song.Time = fullName[2];
+            
+            songs.Add(song);
+        }
+
+        string type = Console.ReadLine();
+
+        switch (type)
+        {
+            case "all":
+                System.Console.WriteLine(string.Join(Environment.NewLine, songs.Select(s => s.Name)));
+                break;
+            default:
+                System.Console.WriteLine(string.Join(Environment.NewLine, songs.Where(s => s.TypeList == type)
+                                                                               .Select(s => s.Name)));
+                break;
+        }
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/03.Songs/Song.cs
@@ -0,0 +1,8 @@
+﻿namespace _03.Songs;
+
+public class Song
+{
+    public string TypeList { get; set; }
+    public string Name { get; set; }
+    public string Time { get; set; }
+}

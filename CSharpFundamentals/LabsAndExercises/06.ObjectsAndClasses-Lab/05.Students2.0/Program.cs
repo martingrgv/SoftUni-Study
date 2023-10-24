02944bde72bb0a172ab5a740680dd1838a8e54a9
Martin <martin.geor04@gmail.com>
2023-10-24 14:02:28 +0300

    Added projects

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/05.Students2.0/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _05.Students2._0;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>();
+        string[] input = Console.ReadLine().Split(" ");
+
+        AddStudents(students, input);
+
+        string city = Console.ReadLine();
+
+        List<Student> homeTownStudents = students.Where(s => s.HomeTown == city).ToList();
+
+        foreach (var student in homeTownStudents)
+        {
+            System.Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+        }
+    }
+
+    public static void AddStudents(List<Student> students, string[] input)
+    {
+        while (input[0] != "end")
+        {
+                Student student = new Student(
+                    input[0],
+                    input[1],
+                    int.Parse(input[2]),
+                    input[3]);
+
+            if (!IsStudentExisting(students, input[0], input[1]))
+            {
+                students.Add(student);
+            }
+            else
+            {
+                // OVERWRITE EXISTING STUDENT!
+            }
+
+            input = Console.ReadLine().Split(" ");
+        }
+    }
+
+    static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+    {
+        foreach (var student in students)
+        {
+            if (student.FirstName == firstName && student.LastName == lastName)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Lab/05.Students2.0/Student.cs
@@ -0,0 +1,15 @@
+﻿ class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string HomeTown { get; set; }
+
+    public Student(string firstName, string lastName, int age, string homeTown)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.HomeTown = homeTown;
+    }
+}

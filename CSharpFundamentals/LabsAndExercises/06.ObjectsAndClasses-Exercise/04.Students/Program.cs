6d223308ba4d0ec38b187f615ad27357992ff48b
Martin <martin.geor04@gmail.com>
2023-10-28 20:45:41 +0300

    Completed 06.OOP-Ex 04.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/04.Students/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                string fname = input[0];
+                string lname = input[1];
+                double grade = double.Parse(input[2]);
+
+                Student currentStudent = new Student(fname, lname, grade);
+                students.Add(currentStudent);
+            }
+
+            students = students.OrderBy(student => student.Grade).ToList();
+            students.Reverse();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/04.Students/Student.cs
@@ -0,0 +1,18 @@
+﻿internal class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+
+    public Student(string firstName, string lastName, double grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}: {Grade:f2}";
+    }
+}

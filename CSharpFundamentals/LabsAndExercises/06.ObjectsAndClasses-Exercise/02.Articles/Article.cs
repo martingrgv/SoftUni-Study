1f8d496d9788766ece02844f19d4e5359765bdcd
Martin <martin.geor04@gmail.com>
2023-10-28 20:45:00 +0300

    Completed 06.OOP-Ex 02.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/02.Articles/Article.cs
@@ -0,0 +1,38 @@
+﻿internal class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public Article()
+    {
+        // Empty constructor
+    }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public void Rename(string title)
+    {
+        Title = title;
+    }
+
+    public void Edit(string content)
+    {
+        Content = content;
+    }
+
+    public void ChangeAuthor(string author)
+    {
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/02.Articles/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArticle = Console.ReadLine().Split(", ");
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            string title = inputArticle[0];
+            string content = inputArticle[1];
+            string author = inputArticle[2];
+
+            Article article = new Article(title, content, author);
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (command[0])
+                {
+                    case "Rename":
+                        string newTitle = command[1];
+                        article.Rename(newTitle);
+                        break;
+                    case "Edit":
+                        string newContent = command[1];
+                        article.Edit(newContent);
+                        break;
+                    case "ChangeAuthor":
+                        string newAuthor = command[1];
+                        article.ChangeAuthor(newAuthor);
+                        break;
+                }
+            }
+
+            Console.WriteLine(article.ToString());
+        }
+    }
+}

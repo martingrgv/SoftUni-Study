1616b400aa0b98a9cc3eab5556509156470c88d2
Martin <martin.geor04@gmail.com>
2023-10-28 20:45:20 +0300

    Completed 06.OOP-Ex 03.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/03.Articles2.0/Article.cs
@@ -0,0 +1,23 @@
+﻿internal class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public Article()
+    {
+        // Empty constructor
+    }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/03.Articles2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Article> articles = new List<Article>();
+        int articlesCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < articlesCount; i++)
+        {
+            string[] inputArticle = Console.ReadLine().Split(", ");
+
+            string title = inputArticle[0];
+            string content = inputArticle[1];
+            string author = inputArticle[2];
+
+            Article currentArticle = new Article(title, content, author);
+            articles.Add(currentArticle);
+        }
+
+        foreach (Article article in articles)
+        {
+            Console.WriteLine(article.ToString());
+        }
+    }
+}

9cd2b47dd3c1d8fccb3642488d632e213d3b7bf8
Martin <martin.geor04@gmail.com>
2023-10-30 15:42:33 +0200

    Completed 06.OOP-Ex 06.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/06.VehicleCatalogue/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _06.VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = GetVehicles();
+
+            PrintVehicleDetails(vehicles);
+            PrintAverageHP(vehicles);
+        }
+
+        static List<Vehicle> GetVehicles()
+        {
+            string input;
+            List<Vehicle> vehicles = new List<Vehicle>();
+            
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] arguments = input.Split(" ");
+                string type = arguments[0];
+                string model = arguments[1];
+                string color = arguments[2];
+                int hp = int.Parse(arguments[3]);
+
+                Vehicle vehicle = new Vehicle(type, model, color, hp);
+                vehicles.Add(vehicle);
+            }
+
+            return vehicles;
+        }
+
+        static Vehicle FindVehicle(string model, List<Vehicle> vehicles)
+        {
+            return vehicles.Find(v => v.Model == model);
+        }
+
+        static void PrintVehicleDetails(List<Vehicle> vehicles)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "Close the Catalogue")
+            {
+                string model = input;
+                Vehicle vehicle = FindVehicle(model, vehicles);
+
+                if (vehicle != null)
+                {
+                    Console.WriteLine(vehicle.ToString());
+                }
+            }
+        }
+
+        static void PrintAverageHP(List<Vehicle> vehicles)
+        {
+            PrintCarsHP(vehicles);
+            PrintTrucksHP(vehicles);
+        }
+
+        static void PrintCarsHP(List<Vehicle> vehicles)
+        {
+            List<Vehicle> cars = vehicles.Where(v => v.Type == "car").ToList();
+            int totalHP = 0;
+            double averageHP = 0;
+
+            if (cars.Count > 0)
+            {
+                cars.ForEach(v => totalHP += v.HP);
+                averageHP = (double) totalHP / cars.Count;
+            }
+
+            Console.WriteLine($"Cars have average horsepower of: {averageHP:f2}.");
+        }
+
+        static void PrintTrucksHP(List<Vehicle> vehicles)
+        {
+            List<Vehicle> trucks = vehicles.Where(v => v.Type == "truck").ToList();
+            int totalHP = 0;
+            double averageHP = 0;
+
+            if (trucks.Count > 0)
+            {
+                trucks.ForEach(v => totalHP += v.HP);
+                averageHP = (double) totalHP / trucks.Count;
+            }
+
+            Console.WriteLine($"Trucks have average horsepower of: {averageHP:f2}.");
+        }
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/06.VehicleCatalogue/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿internal class Vehicle
+{
+    public string Type { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int HP { get; set; }
+
+    public Vehicle(string type, string model, string color, int horsepower)
+    {
+        Type = type;
+        Model = model;
+        Color = color;
+        HP = horsepower;
+    }
+
+    public override string ToString()
+    {
+        return $"Type: {Type[0].ToString().ToUpper()}{Type.Substring(1)}\n" +
+               $"Model: {Model}\n" +
+               $"Color: {Color}\n" +
+               $"Horsepower: {HP}";
+    }
+}

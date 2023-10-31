0eeb30d007b6691b9a423332f498ad79436d1fb2
Martin <martin.geor04@gmail.com>
2023-10-31 15:13:53 +0200

    Uploaded 07.

---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/07.OrderByAge/Person.cs
@@ -0,0 +1,18 @@
+﻿internal class Person
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, string id, int age)
+    {
+        Name = name;
+        Id = id;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} with ID: {Id} is {Age} years old.";
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/06.ObjectsAndClasses-Exercise/07.OrderByAge/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _07.OrderByAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string[] input = Console.ReadLine().Split(" ");
+
+            while (input[0] != "End")
+            {
+                Person p = GetPerson(input);
+                UpdatePerson(people, p);
+
+                input = Console.ReadLine().Split(" ");
+            }
+
+            PrintPeople(people);
+        }
+
+        static Person GetPerson(string[] input)
+        {
+            string name = input[0];
+            string id = input[1];
+            int age = int.Parse(input[2]);
+
+            Person p = new Person(name, id, age);
+
+            return p;
+        }
+
+        static void UpdatePerson(List<Person> people, Person p)
+        {
+            people.Where(person => );
+
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[i].Id == p.Id)
+                {
+                    people[i].Name = p.Name;
+                    people[i].Age = p.Age;
+                }
+                else
+                {
+                    people.Add(p);
+                    break;
+                }
+            }
+        }
+
+        static void PrintPeople(List<Person> people)
+        {
+            people = people.OrderBy(p => p.Age).ToList();
+            
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}

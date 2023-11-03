d9531bb35c1cf1cd4f1a8243374c93e123834e9d
Martin <martin.geor04@gmail.com>
2023-11-03 23:55:10 +0200

    Completed 07.AA 03.

---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            AddToDict(dict);
+            PrintDict(dict);
+        }
+
+        static void AddToDict(Dictionary<string, List<string>> dict)
+        {
+            int wordsCount = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, new());
+                }
+
+                if (dict.ContainsKey(word))
+                {
+                    dict[word].Add(synonym);
+                }
+            }
+        }
+
+        static void PrintDict(Dictionary<string, List<string>> dict)
+        {
+            foreach (KeyValuePair<string, List<string>> word in dict)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}

35629406931d7fe260e871ef31c0f04e0516d749
Martin <martin.geor04@gmail.com>
2023-11-03 23:54:53 +0200

    Completed 07.AA 01.

---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                            .Split()
+                            .Select(double.Parse)
+                            .ToArray();
+
+            SortedDictionary<double, int> dict = new SortedDictionary<double, int>();
+
+            AddToDict(dict, numbers);
+            PrintDict(dict);
+        }
+
+        static void AddToDict(SortedDictionary<double, int> dict, double[] numbers)
+        {
+            foreach (double number in numbers)
+            {
+                if (dict.ContainsKey(number))
+                {
+                    dict[number]++;
+                }
+                else
+                {
+                    dict.Add(number, 1);
+                }
+            }
+        }
+
+        static void PrintDict(SortedDictionary<double, int> dict)
+        {
+            foreach (var num in dict)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}

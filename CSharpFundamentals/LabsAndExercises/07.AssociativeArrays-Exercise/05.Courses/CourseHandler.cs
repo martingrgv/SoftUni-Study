c916dfa7b298b5ab87b49270bee3f88ba2acebac
Martin <martin.geor04@gmail.com>
2023-11-04 23:06:32 +0200

    Completed 07.AA-Ex 05.

---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/05.Courses/CourseHandler.cs
@@ -0,0 +1,40 @@
+﻿ class CourseHandler
+{
+    static Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+    public static void AddCourses()
+    {
+        string input;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] args = input.Split(":", StringSplitOptions.TrimEntries);
+
+            string courseName = args[0];
+            string studentName = args[1];
+
+            if (courses.ContainsKey(courseName))
+            {
+                courses[courseName].Add(studentName);
+            }
+            else
+            {
+                courses.Add(courseName, new());
+                courses[courseName].Add(studentName);
+            }
+        }
+    }
+
+    public static void PrintCourses()
+    {
+        foreach (var course in courses)
+        {
+            Console.WriteLine($"{course.Key}: {course.Value.Count}");
+            
+            foreach (var studentName in course.Value)
+            {
+                Console.WriteLine($"-- {studentName}");
+            }
+        }
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/05.Courses/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _05.Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CourseHandler.AddCourses();
+            CourseHandler.PrintCourses();
+        }
+    }
+}

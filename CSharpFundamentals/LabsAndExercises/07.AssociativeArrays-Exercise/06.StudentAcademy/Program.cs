089ef6d2d5880379dea27590245791c087193a78
Martin <martin.geor04@gmail.com>
2023-11-04 23:07:39 +0200

    Updated class

--- CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/06.StudentAcademy/Program.cs
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/06.StudentAcademy/Program.cs
@@ -1,39 +1,5 @@
 ﻿namespace _06.StudentAcademy
 {
-    class Student
-    {
-        public string Name { get; set; }
-        public List<double> Grades { get; set; }
-
-        public Student()
-        {
-            Grades = new List<double>();
-        }
-
-        public Student(string name)
-        {
-            Name = name;
-        }
-
-        public Student(string name, double grade)
-        {
-            Name = name;
-            Grades = new List<double>();
-            Grades.Add(grade);
-        }
-
-        public double AverageGrade
-        {
-            get
-            {
-                double sum = 0;
-                Grades.ForEach(g => sum += g);
-
-                return sum / Grades.Count;
-            }
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/06.StudentAcademy/Student.cs
@@ -0,0 +1,33 @@
+﻿class Student
+{
+    public string Name { get; set; }
+    public List<double> Grades { get; set; }
+
+    public Student()
+    {
+        Grades = new List<double>();
+    }
+
+    public Student(string name)
+    {
+        Name = name;
+    }
+
+    public Student(string name, double grade)
+    {
+        Name = name;
+        Grades = new List<double>();
+        Grades.Add(grade);
+    }
+
+    public double AverageGrade
+    {
+        get
+        {
+            double sum = 0;
+            Grades.ForEach(g => sum += g);
+
+            return sum / Grades.Count;
+        }
+    }
+}

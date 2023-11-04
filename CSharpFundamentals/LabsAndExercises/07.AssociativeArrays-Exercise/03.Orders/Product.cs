5b1adc5ad9076899f539f2979308474a4f6dc450
Martin <martin.geor04@gmail.com>
2023-11-04 23:06:18 +0200

    Completed 07.AA-Ex 03.

---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/03.Orders/Product.cs
@@ -0,0 +1,26 @@
+﻿internal class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public decimal Quantity { get; set; }
+
+    public Product(string name, decimal price, decimal quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public void Update(decimal price, decimal quantity)
+    {
+        Price = price;
+        Quantity += quantity;
+    }
+
+    public decimal TotalPrice => Price * Quantity;
+
+    public override string ToString()
+    {
+        return $"{Name} -> {TotalPrice:f2}";
+    }
+}
---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/03.Orders/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new Dictionary<string, Product>();
+
+            AddProducts(products);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Value.ToString());
+            }
+        }
+
+        static void AddProducts(Dictionary<string, Product> products)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] arguments = input.Split();
+
+                string productName = arguments[0];
+                decimal productPrice = decimal.Parse(arguments[1]);
+                decimal productQuantity = decimal.Parse(arguments[2]);
+
+                if (products.ContainsKey(productName))
+                {
+                    products[productName].Update(productPrice, productQuantity);
+                }
+                else
+                {
+                    Product product = new Product(productName, productPrice, productQuantity);
+                    products.Add(productName, product);
+                }
+            }
+        }
+    }
+}

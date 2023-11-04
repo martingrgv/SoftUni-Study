0f1c1f8f761f690dc56d6ebbcbd7e0f55858beb3
Martin <martin.geor04@gmail.com>
2023-11-04 23:06:01 +0200

    Completed 07.AA-Ex 01.

---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                            .Split()
+                            .ToArray();
+
+            var occurrencesDict = new Dictionary<char, int>();
+
+            AddTextToDict(occurrencesDict, words);
+            PrintDict(occurrencesDict);
+        }
+
+        static void AddTextToDict(Dictionary<char, int> dict, string[] words)
+        {
+            foreach (var word in words)
+            {
+                foreach (var ch in word)
+                {
+                    if (dict.ContainsKey(ch))
+                    {
+                        dict[ch]++;
+                    }
+                    else
+                    {
+                        dict.Add(ch, 1);
+                    }
+                }
+            }
+        }
+
+        static void PrintDict(Dictionary<char, int> dict)
+        {
+            foreach (var ch in dict)
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+        }
+    }
+}

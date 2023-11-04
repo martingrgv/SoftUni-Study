04e77faecc54fa4bd0e947fc8f5824f0048ef1a6
Martin <martin.geor04@gmail.com>
2023-11-04 23:06:10 +0200

    Completed 07.AA-Ex 02.

---
+++ CSharpFundamentals/LabsAndExercises/07.AssociativeArrays-Exercise/02.MinerTask/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.MinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var resources = new Dictionary<string, int>();
+
+            AddResources(resources);
+            PrintResources(resources);
+        }
+
+        static void AddResources(Dictionary<string, int> resources)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                string resource = input;
+                int quality = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quality;
+                }
+                else
+                {
+                    resources.Add(resource, quality);
+                }
+            }
+        }
+
+        static void PrintResources(Dictionary<string, int> resources)
+        {
+            foreach (var resource in resources)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}

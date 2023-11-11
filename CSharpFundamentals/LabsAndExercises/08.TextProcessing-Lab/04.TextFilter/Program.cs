1cee6a4821afb70cb78da11a2f80f11622f9d411
Martin <martin.geor04@gmail.com>
2023-11-11 02:17:52 +0200

    Completed 08.TextProcessing-Lab 04.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Lab/04.TextFilter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> forbiddenWords = Console.ReadLine().Split(", ").ToList();
+            string text = Console.ReadLine();
+
+            if (forbiddenWords.Any(text.Contains))
+            {
+                forbiddenWords.ForEach(w => text = text.Replace(w, Encrypt(w)));
+            }
+
+            Console.WriteLine(text);
+        }
+
+        static string Encrypt(string word)
+        {
+            string hidden = string.Empty;
+
+            foreach (char ch in word)
+            {
+                hidden += "*";
+            }
+
+            return hidden;
+        }
+    }
+}

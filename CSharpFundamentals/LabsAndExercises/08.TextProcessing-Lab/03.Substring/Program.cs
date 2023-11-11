6efdba017e7de420999eee38a1e739f20285aa52
Martin <martin.geor04@gmail.com>
2023-11-11 02:17:45 +0200

    Completed 08.TextProcessing-Lab 03.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Lab/03.Substring/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string sentance = Console.ReadLine();
+
+            while (sentance.Contains(wordToRemove))
+                sentance = sentance.Replace(wordToRemove, "");
+
+            Console.WriteLine(sentance);
+        }
+    }
+}

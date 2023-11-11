2160240c392193e7117b35f7d6eab25e55635f15
Martin <martin.geor04@gmail.com>
2023-11-11 02:17:27 +0200

    Completed 08.TextProcessing-Lab 01.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                sb.AppendLine(input + " = " + string.Join("", input.Reverse()));
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

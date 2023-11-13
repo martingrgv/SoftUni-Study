fa42284b4814f00693969c092f99d5280dc1613f
Martin <martin.geor04@gmail.com>
2023-11-14 01:14:37 +0200

    Completed 08.TextProcessing-Ex 01.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            string[] validUsernames = GetValidUsernames(usernames);
+
+            Console.WriteLine(string.Join("\n", validUsernames));
+        }
+
+        static string[] GetValidUsernames(string[] usernames)
+        {
+            string[] validLenghtUsernames = usernames.Where(u => u.Length >= 3 && u.Length <= 16).ToArray();
+            return validLenghtUsernames.Where(u => u.All(ch => char.IsLetterOrDigit(ch) || ch == '-' || ch == '_')).ToArray();
+        }
+    }
+}

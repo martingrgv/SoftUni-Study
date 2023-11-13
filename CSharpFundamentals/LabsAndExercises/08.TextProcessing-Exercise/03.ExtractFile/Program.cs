58366bc36cf939455790e0cc1dcdb84f304ef1fb
Martin <martin.geor04@gmail.com>
2023-11-14 01:14:49 +0200

    Completed 08.TextProcessing-Ex 03.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/03.ExtractFile/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            string fileExtension = Path.GetExtension(path).Replace(".", "");
+
+            Console.WriteLine("File name: " + fileName);
+            Console.WriteLine("File extension: " + fileExtension);
+        }
+    }
+}

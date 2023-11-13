dba7c7948ba22907b93b3a706efd47d7c76029fa
Martin <martin.geor04@gmail.com>
2023-11-14 01:14:42 +0200

    Completed 08.TextProcessing-Ex 02.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string firstWord = input[0];
+            string lastWord = input[1];
+
+            int sum = Sum(firstWord, lastWord);
+
+            Console.WriteLine(sum);
+        }
+
+        private static int Sum(string firstWord, string lastWord)
+        {
+            int fLength = firstWord.Length;
+            int lLength = lastWord.Length;
+            int length = Math.Max(firstWord.Length, lastWord.Length);
+
+            int sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i >= fLength) { sum += lastWord[i]; } else
+                if (i >= lLength) { sum += firstWord[i]; }
+                else { sum += firstWord[i] * lastWord[i]; }
+            }
+
+            return sum;
+        }
+    }
+}

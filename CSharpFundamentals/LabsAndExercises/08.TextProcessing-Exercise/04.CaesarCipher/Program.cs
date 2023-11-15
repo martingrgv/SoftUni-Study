7b9d487abc40a713c56eea1673715aaf8ba4de1b
Martin <martin.geor04@gmail.com>
2023-11-15 19:27:01 +0200

    Compled 08.TextProcessing-Ex 04.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/04.CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string encryptedMessage = string.Empty;
+
+            foreach (char ch in input)
+            {
+                encryptedMessage += SwitchChar(ch);
+            }
+
+            Console.WriteLine(encryptedMessage);
+        }
+
+        static char SwitchChar(char ch)
+        {
+            return (char)(ch + 3);
+        }
+    }
+}

8b85dee956e67979da6c65d81381a1b5fe96544d
Martin <martin.geor04@gmail.com>
2023-11-15 19:27:10 +0200

    Compled 08.TextProcessing-Ex 05.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNum = Console.ReadLine();
+            string multiplyer = Console.ReadLine();
+
+            Console.WriteLine(MultiplyBigNumbers(bigNum, multiplyer));
+        }
+
+        private static string MultiplyBigNumbers(string number, string multiplyerNumber)
+        {
+            if (number == "0" || multiplyerNumber == "0")
+            {
+                return "0";
+            }
+
+            int carry = 0;
+            int multiplyer = int.Parse(multiplyerNumber);
+
+            char[] resultChar = new char[number.Length + 1];
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(number[i].ToString());
+                int product = digit * multiplyer + carry;
+
+                resultChar[i + 1] = (char)(product % 10 + '0');
+                carry = product / 10;
+            }
+
+            if (carry > 0)
+            {
+                resultChar[0] = (char)(carry + '0');
+            }
+
+            return new string (resultChar).TrimStart('\0');
+        }
+    }
+}

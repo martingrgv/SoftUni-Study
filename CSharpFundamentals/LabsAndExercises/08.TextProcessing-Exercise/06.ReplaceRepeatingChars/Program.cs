a2cd4f272b7328c997d05d47ba2aa7324dbafaac
Martin <martin.geor04@gmail.com>
2023-11-15 19:27:17 +0200

    Compled 08.TextProcessing-Ex 06.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string word = RemoveRepeatingChars(input);
+
+            Console.WriteLine(word);
+        }
+
+        static string RemoveRepeatingChars(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(word[0]);
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] != word[i - 1])
+                {
+                    sb.Append(word[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}

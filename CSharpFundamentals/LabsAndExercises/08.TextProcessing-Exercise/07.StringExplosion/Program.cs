9fce1cb97bb2f56b45faed2426c53f7ff52a3e88
Martin <martin.geor04@gmail.com>
2023-11-17 03:17:19 +0200

    Completed 08.TextProcessing-Ex 07.

---
+++ CSharpFundamentals/LabsAndExercises/08.TextProcessing-Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string explosionString = Console.ReadLine();
+            string explodedString = ExplodeString(explosionString);
+
+            Console.WriteLine(explodedString);
+        }
+
+        private static string ExplodeString(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            int explosionStrenght = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '>')
+                {
+                    explosionStrenght += int.Parse(str[i + 1].ToString());
+                    sb.Append(str[i]);
+                }
+                else if (explosionStrenght == 0)
+                {
+                    sb.Append(str[i]);
+                }
+                else
+                {
+                    explosionStrenght--;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}

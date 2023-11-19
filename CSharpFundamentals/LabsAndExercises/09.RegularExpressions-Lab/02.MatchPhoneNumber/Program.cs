ff27aa68bac63aaf894dde0c0dd461ef7e13ee71
Martin <martin.geor04@gmail.com>
2023-11-19 19:34:55 +0200

    Completed 09.Regex 02.

---
+++ CSharpFundamentals/LabsAndExercises/09.RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+            var matchedPhones = matches.Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}

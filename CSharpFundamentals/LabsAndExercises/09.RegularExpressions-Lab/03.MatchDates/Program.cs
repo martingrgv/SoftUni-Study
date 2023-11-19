15cf12981cd3f85d43589dad98de53e2814c0734
Martin <martin.geor04@gmail.com>
2023-11-19 19:35:01 +0200

    Completed 09.Regex 03.

---
+++ CSharpFundamentals/LabsAndExercises/09.RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})(.)(?<month>[A-Z][a-z]{2})(\1)(?<year>\d{4})\b";
+            string datesInput = Console.ReadLine();
+
+            var dates = Regex.Matches(datesInput, pattern);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}

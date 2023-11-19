c9ad3695545cf86588c27477cb6efd3aa1fb56f1
Martin <martin.geor04@gmail.com>
2023-11-19 19:34:48 +0200

    Completed 09.Regex 01.

---
+++ CSharpFundamentals/LabsAndExercises/09.RegularExpressions-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"\b(?<FirstName>[A-Z][a-z]+) (?<LastName>[A-Z][a-z]+)\b";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+        }
+    }
+}

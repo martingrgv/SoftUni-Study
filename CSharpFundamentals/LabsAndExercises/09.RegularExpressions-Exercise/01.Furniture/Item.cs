2f54ca0b34c0ff99447d5c2514303c93e68e46c6
Martin <martin.geor04@gmail.com>
2023-11-19 20:22:22 +0200

    Completed 09.Regex-Ex 01.

---
+++ CSharpFundamentals/LabsAndExercises/09.RegularExpressions-Exercise/01.Furniture/Item.cs
@@ -0,0 +1,7 @@
+﻿class Item
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public decimal Total { get { return Price * Quantity; } }
+}
---
+++ CSharpFundamentals/LabsAndExercises/09.RegularExpressions-Exercise/01.Furniture/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> furnitures = new List<Item>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Item furniture = GetItem(input);
+
+                if (furniture.Name != null)
+                    furnitures.Add(furniture);
+            }
+
+            PrintFurnitures(furnitures);
+        }
+        
+        private static Item GetItem(string input)
+        {
+            string pattern = @">>(?<Name>[A-Za-z]+)<<(?<Price>\d+\.\d+|\d+)!(?<Quantity>\d+)";
+
+            foreach (Match match in Regex.Matches(input, pattern))
+            {
+                Item furniture = new Item();
+                furniture.Name = match.Groups["Name"].Value;
+                furniture.Price = decimal.Parse(match.Groups["Price"].Value);
+                furniture.Quantity = int.Parse(match.Groups["Quantity"].Value);
+
+                return furniture;
+            }
+
+            return new Item();
+        }
+
+        private static void PrintFurnitures(List<Item> furnitures)
+        {
+            decimal total = 0;
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (Item furniture in furnitures)
+            {
+                Console.WriteLine(furniture.Name);
+                total += furniture.Total;
+            }
+
+            Console.WriteLine($"Total money spend: {total:f2}");
+        }
+    }
+}

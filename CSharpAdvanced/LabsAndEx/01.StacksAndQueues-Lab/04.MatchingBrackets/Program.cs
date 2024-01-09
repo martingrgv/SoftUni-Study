1365b8f4f3d649ea5596f2cd312de1f4b443f355
Martin <martin.geor04@gmail.com>
2024-01-10 00:12:35 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            var openingBracketIndexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openingBracketIndexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int startIndex = openingBracketIndexes.Pop();
+                    int endIndex = i;
+
+                    string substring = expression.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}

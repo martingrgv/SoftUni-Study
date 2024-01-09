e6652b73a4504245bd6312335eadd5a3c9110d7e
Martin <martin.geor04@gmail.com>
2024-01-09 20:13:33 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/1.ReverseAString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            // Add to stack
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            // Read stack
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }    
+        }
+    }
+}

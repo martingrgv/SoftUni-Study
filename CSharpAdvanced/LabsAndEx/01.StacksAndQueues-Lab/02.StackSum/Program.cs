accfb5e22e3859d28286db482764c452a03ddf85
Martin <martin.geor04@gmail.com>
2024-01-09 20:32:29 +0200

    Complete ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/02.StackSum/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string command;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] arguments = command.Split();
+
+                switch (arguments[0].ToLower())
+                {
+                    case "add":
+                        stack.Push(int.Parse(arguments[1]));
+                        stack.Push(int.Parse(arguments[2]));
+                        break;
+                    case "remove":
+                        int count = int.Parse(arguments[1]);
+
+                        if (stack.Count >= count)
+                        {
+                            while (count > 0) 
+                            {
+                                stack.Pop();
+                                count--;
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            // Print sum of elements
+            int sum = 0;
+
+            while (stack.Count > 0)
+                sum += stack.Pop();
+
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}

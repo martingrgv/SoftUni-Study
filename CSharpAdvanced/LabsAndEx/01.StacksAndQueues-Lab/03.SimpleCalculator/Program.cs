eef727e0842a7a1bafe1ff05f721174417a71330
Martin <martin.geor04@gmail.com>
2024-01-09 23:37:43 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] calculator = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Array.Reverse(calculator);
+            var stack = new Stack<string>(calculator);
+
+            int sum = int.Parse(stack.Pop());
+
+            while (stack.Count > 0) // Loop through acts
+            {
+                string @operator = stack.Pop();
+                int num = int.Parse(stack.Pop());
+
+                switch(@operator) // Do math operation
+                {
+                    case "+":
+                        sum += num;
+                        break;
+                    case "-":
+                        sum -= num;
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}

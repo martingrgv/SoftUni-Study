bb4b37a8cd54817dd5baad8fb492f4e2f0eb4bb1
Martin <martin.geor04@gmail.com>
2024-01-10 21:59:24 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsPassingCount = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+
+            int passedCars = 0;
+
+            string input;
+            while ((input = Console.ReadLine()) != "end") // Read cars
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < carsPassingCount; i++)
+                    {
+                        if (cars.Count <= 0)
+                            break;
+
+                        Console.WriteLine(cars.Dequeue() + " passed!");
+                        passedCars++;
+                    }
+                }
+                else // Add car
+                {
+                    cars.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine(passedCars + " cars passed the crossroads.");
+        }
+    }
+}

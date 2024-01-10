f8f31ca7c335a2f417dcbdd0151868ff6e90cade
Martin <martin.geor04@gmail.com>
2024-01-10 14:29:58 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/07.HotPotato/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> children = new Queue<string>(names);
+
+            while (children.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    string currentChild = children.Dequeue();
+                    children.Enqueue(currentChild);
+                }
+
+                Console.WriteLine("Removed " + children.Dequeue());
+            }
+
+            Console.WriteLine("Last is " + children.Dequeue());
+        }
+    }
+}

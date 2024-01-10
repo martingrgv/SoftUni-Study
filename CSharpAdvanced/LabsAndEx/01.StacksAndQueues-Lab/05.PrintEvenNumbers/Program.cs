5f59ab351387258403c61c3de9b510a0c154ee8f
Martin <martin.geor04@gmail.com>
2024-01-10 13:29:42 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(numbers);
+
+            int number;
+
+            while (queue.Count > 1)
+            {
+                number = queue.Dequeue();
+
+                if (number % 2 == 0)
+                {
+                    Console.Write(number + ", ");
+                }
+            }
+
+            number = queue.Dequeue();
+
+            if (number % 2 == 0)
+            {
+                Console.Write(number);
+            }
+        }
+    }
+}

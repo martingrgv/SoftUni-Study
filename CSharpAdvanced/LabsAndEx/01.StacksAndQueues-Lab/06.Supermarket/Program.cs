1b49ef484f44641fedeec7ce84d7daadbb754a3e
Martin <martin.geor04@gmail.com>
2024-01-10 13:38:26 +0200

    Completed ex

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Lab/06.Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> people = new Queue<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    RemovePeople(people);
+                }
+                else // Add person to queue
+                {
+                    people.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine(people.Count + " people remaining.");
+        }
+
+        static void RemovePeople(Queue<string> people)
+        {
+            while (people.Count > 0)
+            {
+                Console.WriteLine(people.Dequeue());
+            }
+        }
+    }
+}

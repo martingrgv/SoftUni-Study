1491e384dc0ce5c72759a6a6924492981e2486b6
Martin <martin.geor04@gmail.com>
2024-01-12 21:07:38 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Exercise/04.FastFood/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] orderQuantity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>(orderQuantity);
+
+            int biggestOrder = 0;
+
+            while (orders.Count > 0)
+            {
+                int firstOrder = orders.Peek();
+
+                if (biggestOrder < firstOrder) // Get biggest order
+                {
+                    biggestOrder = firstOrder;
+                }
+
+                if (foodQuantity - firstOrder >= 0) // Serve customer
+                {
+                    int order = orders.Dequeue();
+                    foodQuantity -= order;
+                }
+                else
+                {
+                    Console.WriteLine(biggestOrder);
+                    Console.Write("Orders left: ");
+
+                    while (orders.Count > 0)
+                    {
+                        Console.Write(orders.Dequeue() + " ");
+                    }
+
+                    return;
+                }
+            }
+
+            Console.WriteLine(biggestOrder);
+            Console.WriteLine("Orders complete");
+        }
+    }
+}

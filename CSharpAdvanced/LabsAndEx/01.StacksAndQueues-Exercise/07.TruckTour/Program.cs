9cf8140d903fabf041bc9d16c1a03000caaa0fb9
Martin <martin.geor04@gmail.com>
2024-01-13 16:19:18 +0200

    Completed Ex.

--- CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Exercise/07.TruckTour/Program.cs
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Exercise/07.TruckTour/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace _07.TruckTour
+﻿using System.ComponentModel;
+
+namespace _07.TruckTour
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            Queue<int[]> pumpsQueue = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++) // Add pumps to queue
+            {
+                int[] pumpData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                pumpsQueue.Enqueue(pumpData);
+            }
+
+            int position = 0;
+            while (true)
+            {
+                int fuel = 0;
+
+                foreach (var pump in pumpsQueue)
+                {
+                    fuel += pump[0] - pump[1];
+                    
+                    if (fuel < 0)
+                    {
+                        position++;
+                        pumpsQueue.Enqueue(pumpsQueue.Dequeue());
+                        break;
+                    }
+                }
+
+                if (fuel >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(position);
         }
     }
 }

7d1cb7b71d6467afee5d9c1ddb03f253e2eac8ea
Martin <martin.geor04@gmail.com>
2024-01-13 15:22:50 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesRange = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> clothBox = new Stack<int>(clothesRange);
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int racksUsed = 0;
+
+            while (clothBox.Count > 0)
+            {
+                int clothesToHang = clothBox.Pop();
+
+                while (clothBox.Count > 0 && clothesToHang + clothBox.Peek() <= rackCapacity)
+                {
+                        clothesToHang += clothBox.Pop();                   
+                }
+
+                racksUsed++;
+            }
+
+            Console.WriteLine(racksUsed);
+        }
+    }
+}

467a703abf30de7c16278fd79a334e89dea4f158
Martin <martin.geor04@gmail.com>
2024-01-13 15:22:54 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+            Queue<string> songsQueue = new Queue<string>(songs);
+
+            while (songsQueue.Count > 0)
+            {
+                string command = Console.ReadLine();
+                string[] tokens = command.Split();
+
+                switch (tokens[0])
+                {
+                    case "Play":
+                        songsQueue.Dequeue();
+                        break;
+                    case "Add":
+                        string songName = string.Join(' ', tokens.Skip(1));
+
+                        if (!songsQueue.Contains(songName))
+                        {
+                            songsQueue.Enqueue(songName);
+                        }
+                        else
+                        {
+                            Console.WriteLine(songName + " is already contained!");
+                        }
+                        break;
+                    case "Show":
+                        songs = songsQueue.ToArray();
+                        Console.WriteLine(string.Join(", ", songs));
+                        break;
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}

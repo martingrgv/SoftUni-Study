6a6e21a71b87dc1e11872330f1a41d2a89232159
Martin <martin.geor04@gmail.com>
2024-01-14 23:05:36 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/02.MultidimensionalArrays-Lab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] size = Console.ReadLine().Split(", ");
+
+            int rows = int.Parse(size[0]);
+            int cols = int.Parse(size[1]);
+
+            int[,] matrix = ReadNumberMatrix(rows, cols, " ");
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+
+        }
+        private static int[,] ReadNumberMatrix(int rows, int cols, string separator)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] numbers = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}

f79f0529f59988c9bc9d2e42c7e20a93d6c2c9f3
Martin <martin.geor04@gmail.com>
2024-01-16 02:22:55 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/02.MultidimensionalArrays-Exercises/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = size[0];
+            int cols = size[1];
+
+            string[,] matrix = ReadMatrix(rows, cols);
+
+            int squareCount = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col + 1] == matrix[row + 1, col + 1])
+                    {
+                        squareCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squareCount);
+        }
+
+        private static string[,] ReadMatrix(int rows, int cols, string separator = " ")
+        {
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] str = Console.ReadLine().Split(separator);
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = str[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}

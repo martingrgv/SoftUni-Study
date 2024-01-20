c3d6d80bcf80cb49e8606a934f2a3c98652d1058
Martin <martin.geor04@gmail.com>
2024-01-20 16:34:58 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvances-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> occurrences = new Dictionary<double, int>();
+
+            // Populate dict
+            for (int i = 0; i < numbers.Length; i++) // Loop through array
+            {
+                double number = numbers[i];
+
+                if (occurrences.ContainsKey(number))
+                {
+                    occurrences[number]++;
+                }
+                else
+                {
+                    occurrences.Add(number, 1);
+                }
+            }
+
+            // Print 
+            foreach (var number in occurrences)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}

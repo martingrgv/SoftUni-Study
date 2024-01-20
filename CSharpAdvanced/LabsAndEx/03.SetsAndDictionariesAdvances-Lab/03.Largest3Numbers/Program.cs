60f3938c74a81f69123484c636fcc2fb099137e3
Martin <martin.geor04@gmail.com>
2024-01-20 19:59:57 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvances-Lab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> orderedList = list.OrderByDescending(num => num).ToList();
+
+            if (orderedList.Count < 3)
+            {
+                Console.WriteLine(string.Join(' ', orderedList));
+                return;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(orderedList[i] + " ");
+            }
+        }
+    }
+}

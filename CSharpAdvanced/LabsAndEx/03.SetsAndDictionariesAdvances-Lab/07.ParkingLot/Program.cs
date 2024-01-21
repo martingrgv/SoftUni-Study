e293b109a4b2d157e501f4d09c4ee86dadfdd2aa
Martin <martin.geor04@gmail.com>
2024-01-21 12:14:03 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvances-Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = commandArgs[0];
+                string carLicense = commandArgs[1];
+
+                if (direction == "OUT")
+                {
+                    if (parkingLot.Contains(carLicense))
+                    {
+                        parkingLot.Remove(carLicense);
+                    }
+
+                    continue;
+                }
+
+                parkingLot.Add(carLicense);
+            }
+
+            if (parkingLot.Count <= 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+
+            foreach (var car in parkingLot)
+                Console.WriteLine(car);
+        }
+    }
+}

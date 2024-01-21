227f03c57c8cd2cac0e3eebc8a55155bc6eba655
Martin <martin.geor04@gmail.com>
2024-01-21 15:27:28 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/01.StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _08.BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sequence = Console.ReadLine().ToCharArray();
+
+            if (sequence.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            Stack<char> charStack = new Stack<char>();
+
+            foreach (char ch in sequence)
+            {
+                if ("{([".Contains(ch)) // Opening
+                {
+                    charStack.Push(ch);
+                }
+                else if (ch == ')' && charStack.Peek() == '(')
+                {
+                    charStack.Pop();
+                }
+                else if (ch == ']' && charStack.Peek() == '[')
+                {
+                    charStack.Pop();
+                }
+                else if (ch == '}' && charStack.Peek() == '{')
+                {
+                    charStack.Pop();
+                }
+            }
+
+            Console.WriteLine(charStack.Any() ? "NO" : "YES");
+        }
+    }
+}

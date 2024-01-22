f0928857a90570bee05e74eeed3b4bb7f0923dc7
Martin <martin.geor04@gmail.com>
2024-01-22 19:05:11 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentElements = Console.ReadLine().Split();
+
+                for (int j = 0; j < currentElements.Length; j++)
+                {
+                    elements.Add(currentElements[j]);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', elements));
+        }
+    }
+}

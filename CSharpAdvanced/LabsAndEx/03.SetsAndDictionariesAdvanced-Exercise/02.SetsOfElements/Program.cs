beb3f66986d035527818e88af9f28f404bad16a0
Martin <martin.geor04@gmail.com>
2024-01-22 19:02:16 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvanced-Exercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int set1Size = size[0];
+            int set2Size = size[1];
+
+            for (int i = 0; i < set1Size; i++)
+                set1.Add(int.Parse(Console.ReadLine()));
+
+            for (int i = 0; i < set2Size; i++)
+                set2.Add(int.Parse(Console.ReadLine()));
+
+            set1.IntersectWith(set2);
+
+            Console.WriteLine(String.Join(' ', set1));
+        }
+    }
+}

4dedb802479e77195d5befc841f64638f582ed18
Martin <martin.geor04@gmail.com>
2024-01-22 19:51:58 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvanced-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> chars = new SortedDictionary<char, int>();
+
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+
+                if (!chars.ContainsKey(ch))
+                {
+                    chars.Add(ch, 0);
+                }
+
+                chars[ch]++;
+            }
+
+            foreach (var ch in chars)
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+        }
+    }
+}

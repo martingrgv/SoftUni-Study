17b79a5b9709a744d3d41331c5397fb62b0b4a9a
Martin <martin.geor04@gmail.com>
2024-01-22 19:05:17 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvanced-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currentNum))
+                {
+                    numbers.Add(currentNum, 0);
+                }
+
+                numbers[currentNum]++;
+            }
+
+            foreach (var number in numbers)
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                }
+            }
+        }
+    }
+}

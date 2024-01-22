850f3d7976fc5902634578dccb083bb51335db67
Martin <martin.geor04@gmail.com>
2024-01-22 19:05:05 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/03.SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+                usernames.Add(Console.ReadLine());
+
+            Console.WriteLine(String.Join("\n", usernames));
+        }
+    }
+}

56c0e6e46f3406c9a26340a12ee6d249e157f68e
Martin <martin.geor04@gmail.com>
2024-01-25 21:26:27 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = input.Split(new string[] { ", " },
+                               StringSplitOptions.RemoveEmptyEntries)
+                               .Select(parser)
+                               .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

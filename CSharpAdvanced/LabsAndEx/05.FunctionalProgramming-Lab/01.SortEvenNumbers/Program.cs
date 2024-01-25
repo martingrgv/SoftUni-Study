cb020c60fd6237302b57e04fc54ea0d2b0259685
Martin <martin.geor04@gmail.com>
2024-01-25 21:26:17 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.SortEvennbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenNumbers = Console.ReadLine()
+                                .Split(", ",
+                                StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .Where(n => n % 2 == 0)
+                                .OrderBy(n => n)
+                                .ToArray();
+
+            Console.WriteLine(String.Join(", ", evenNumbers));
+        }
+    }
+}

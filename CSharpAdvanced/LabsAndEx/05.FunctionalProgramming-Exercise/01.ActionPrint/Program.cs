8f31c7dc08ed281c49315eb57117028b2aa3f32c
Martin <martin.geor04@gmail.com>
2024-01-26 20:40:31 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strCollection = Console.ReadLine()
+                                     .Split(' ',
+                                     StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> printer = s => Console.WriteLine(s);
+
+            foreach (var str in strCollection)
+            {
+                printer(str);
+            }
+        }
+    }
+}

9519f29d06db4dca150bde07d2fcd148af0e15a6
Martin <martin.geor04@gmail.com>
2024-01-26 20:40:41 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/02.KnightsOfHorror/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.KnightsOfHorror
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                             .Split(' ',
+                             StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, string> appender = s => s = "Sir " + s;
+            Action<string> printer = s => Console.WriteLine(s);
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string name = names[i];
+
+                printer(appender(name));
+            }
+        }
+    }
+}

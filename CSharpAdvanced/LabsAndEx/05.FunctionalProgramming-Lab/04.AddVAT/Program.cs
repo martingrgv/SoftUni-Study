d99682de5846a9a7a2fb7c9568edcf435b7b2571
Martin <martin.geor04@gmail.com>
2024-01-26 13:26:14 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVat = (double x) => x + x * 0.2;
+
+            double[] numbers = Console.ReadLine()
+                            .Split(", ",
+                             StringSplitOptions.RemoveEmptyEntries)
+                            .Select(double.Parse)
+                            .Select(n => addVat(n))
+                            .ToArray();
+
+            Action<double> print = n => Console.WriteLine($"{n:f2}");
+
+            for (int i = 0; i < numbers.Length; i++)
+                print(numbers[i]);
+        }
+    }
+}

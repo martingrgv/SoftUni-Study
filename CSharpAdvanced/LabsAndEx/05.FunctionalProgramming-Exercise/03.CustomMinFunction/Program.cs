ffda14de0838d59bb2e0b4d7e6877e67fe3bb6a9
Martin <martin.geor04@gmail.com>
2024-01-26 20:40:48 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                             .Split(' ',
+                             StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+
+            int smallestNum = int.MaxValue;
+
+            Func<int, int> NumIdentifier = n => n < smallestNum ? n : smallestNum;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+                smallestNum = NumIdentifier(number);
+            }
+
+            Console.WriteLine(smallestNum);
+
+        }
+    }
+}

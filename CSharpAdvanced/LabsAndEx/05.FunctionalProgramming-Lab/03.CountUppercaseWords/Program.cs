64ee1ea6cc3d58dcb972e8bc31166061ceaa5f67
Martin <martin.geor04@gmail.com>
2024-01-26 12:56:16 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isUppercase = w => char.IsUpper(w[0]);
+
+            string[] text = Console.ReadLine()
+                          .Split(" ",
+                          StringSplitOptions.RemoveEmptyEntries)
+                          .Where(w => isUppercase(w))
+                          .ToArray();
+
+            Console.WriteLine(string.Join('\n', text));
+        }
+    }
+}

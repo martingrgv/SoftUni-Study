e20e8cb86d2aeb7832635976f1652c16c751c14b
Martin <martin.geor04@gmail.com>
2024-01-27 16:11:33 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/04.FindEvenOrOdds/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _04.FindEvenOrOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+            int startRange = range[0];
+            int endRange = range[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int i = startRange; i <= endRange; i++) // Populating list with all numbers
+            {
+                numbers.Add(i);
+            }
+
+            string type = Console.ReadLine();
+
+            Predicate<int> predicate;
+
+            if (type == "even")
+            {
+                predicate = n => n % 2 == 0;
+            }
+            else
+            {
+                predicate = n => n % 2 != 0;
+            }
+
+            var filteredNumbers = numbers.Where(n => predicate(n));
+
+            Console.WriteLine(string.Join(' ', filteredNumbers));
+        }
+    }
+}

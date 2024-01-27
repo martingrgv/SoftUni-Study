a6398a4d3513f510c0c2f7c887e67e8a1af33c64
Martin <martin.geor04@gmail.com>
2024-01-27 16:33:29 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            Predicate<int> predicate = n => n % divisibleNum == 0;
+
+            numbers.RemoveAll(predicate);
+            numbers.Reverse();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

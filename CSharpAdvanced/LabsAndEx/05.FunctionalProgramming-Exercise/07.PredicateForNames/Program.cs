af984f6c02ab4a2bd10d374b86d1a049875ae405
Martin <martin.geor04@gmail.com>
2024-01-27 16:40:05 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                             .Split()
+                             .Where(n => n.Length <= length)
+                             .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}

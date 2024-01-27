19d013f6ff004c6a40109d35c6aa9c5e355c5fac
Martin <martin.geor04@gmail.com>
2024-01-27 16:17:12 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split()
+                            .Select(int.Parse)
+                            .ToArray();
+
+            Func<int, int> act = n => n;
+            Action<int[]> print = n => Console.WriteLine(string.Join(' ', n));
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        act = n => ++n;                       
+                        break;
+                    case "subtract":
+                        act = n => --n;                       
+                        break;
+                    case "multiply":
+                        act = n => n * 2;
+                        break;
+                    case "print":
+                        print(numbers);
+                        continue;
+                }
+
+                numbers = numbers.Select(n => act(n)).ToArray();
+            }
+        }
+    }
+}

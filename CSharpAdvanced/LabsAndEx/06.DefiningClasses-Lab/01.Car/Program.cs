ce5f1ed586166b77c0fd0c263a43b116d20fb1fa
Martin <martin.geor04@gmail.com>
2024-01-30 18:13:08 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Lab/01.Car/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new();
+
+            car.Make = "Subaru";
+            car.Model = "Impreza";
+            car.Year = 2009;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}

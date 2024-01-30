740dc88c590470a24ca442dc31be74c176366a10
Martin <martin.geor04@gmail.com>
2024-01-30 17:53:39 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/05.FunctionalProgramming-Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                                 .Split(' ',
+                                 StringSplitOptions.RemoveEmptyEntries)
+                                 .ToList();
+
+            // Logic
+            Func<string, int, bool> predicate = (name, n) =>
+            {
+                int sum = 0;
+
+                foreach (char ch in name)
+                    sum += ch;
+
+                if (sum >= n)
+                    return true;
+                else
+                    return false;
+            };
+
+            Func<List<string>, int, Func<string, int, bool>, string> GetName = (names, n, predicate) =>
+            {
+                foreach (var name in names)
+                {
+                    if (predicate(name, n))
+                        return name;
+                }
+
+                return null;
+            };
+
+            string firstFoundName = GetName(names, n, predicate);
+
+            // Output
+            if (firstFoundName != null)
+                Console.WriteLine(firstFoundName);
+        }
+    }
+}

75f5c70ecbe016ded18ba96eb1c20374eded9f04
Martin <martin.geor04@gmail.com>
2024-02-03 16:56:07 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        Person p2 = new Person(20);
+        Person p3 = new Person("Martin", 20);
+
+        Console.WriteLine(p1.Name + '|' + p1.Age);
+        Console.WriteLine(p2.Name + '|' + p2.Age);
+        Console.WriteLine(p3.Name + '|' + p3.Age);
+    }
+}

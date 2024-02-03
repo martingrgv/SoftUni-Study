6d12ea4d673e0711538ca34df5733c3fbc177c38
Martin <martin.geor04@gmail.com>
2024-02-04 01:09:03 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiningClasses;
+
+public static class DateModifier
+{
+    public static int GetDateDifference(string date1, string date2)
+    {
+        var dateTime1 = DateTime.Parse(date1);
+        var dateTime2 = DateTime.Parse(date2);
+
+        var difference = dateTime1 - dateTime2;
+
+        return difference.Duration().Days;
+    }
+}
---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        string date1 = Console.ReadLine();
+        string date2 = Console.ReadLine();
+
+        var difference = DateModifier.GetDateDifference(date1, date2);
+        Console.WriteLine(difference);
+    }
+}

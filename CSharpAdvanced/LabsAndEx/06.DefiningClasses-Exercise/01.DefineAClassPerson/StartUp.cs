0fcfe082ce2d74a4832672c13ddf94947b44c1a4
Martin <martin.geor04@gmail.com>
2024-02-03 16:55:58 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        Person p2 = new Person();
+        Person p3 = new Person();
+
+        p1.Name = "Peter";
+        p2.Name = "George";
+        p3.Name = "Jose";
+
+        p1.Age = 20;
+        p2.Age = 18;
+        p3.Age = 43;
+    }
+}

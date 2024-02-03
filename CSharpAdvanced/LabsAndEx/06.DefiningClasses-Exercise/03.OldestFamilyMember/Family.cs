8b5e908a03462e33753b6c08ad67657491d6fe08
Martin <martin.geor04@gmail.com>
2024-02-03 16:56:17 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,24 @@
+﻿namespace DefiningClasses;
+
+public class Family
+{
+    private List<Person> people;
+
+    public Family()
+    {
+        People = new List<Person>();
+    }
+
+    public List<Person> People
+    {
+        get { return this.people; }
+        set { this.people = value; }
+    }
+
+    public void AddMember(Person member)
+    {
+        people.Add(member);
+    }
+
+    public Person GetOldestMember() => people.MaxBy(p => p.Age);
+}
---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var family = new Family();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] personArgs = Console.ReadLine().Split();
+
+            string name = personArgs[0];
+            int age = int.Parse(personArgs[1]);
+
+            family.AddMember(new Person(name, age));
+        }
+
+        Person oldestPerson = family.GetOldestMember();
+
+        Console.WriteLine(oldestPerson.Name + " " + oldestPerson.Age);
+    }
+}

20575de8121b18f5bc16cb260e24e8f73d4554bd
Martin <martin.geor04@gmail.com>
2024-02-04 12:56:01 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Text;
+
+namespace DefiningClasses;
+
+public class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double fuelConsumptionPerKilometer;
+    private double travelledDistance;
+
+    public Car()
+    {
+        this.TravelledDistance = 0;
+    }
+
+    public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer) : this()
+    {
+        this.Model = model;
+        this.FuelAmount = fuelAmount;
+        this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+    }
+
+    public string Model
+    {
+        get { return this.model; }
+        set { this.model = value; }
+    }
+
+    public double FuelAmount
+    {
+        get { return this.fuelAmount; }
+        set { this.fuelAmount = value; }
+    }
+
+    public double FuelConsumptionPerKilometer
+    {
+        get { return this.fuelConsumptionPerKilometer; }
+        set { this.fuelConsumptionPerKilometer = value; }
+    }
+
+    public double TravelledDistance
+    {
+        get { return this.travelledDistance; }
+        set { this.travelledDistance = value; }
+    }
+
+    public void Drive(double distance)
+    {
+        if (this.FuelAmount - (distance * this.FuelConsumptionPerKilometer) >= 0)
+        {
+            this.FuelAmount -= distance * this.FuelConsumptionPerKilometer;
+            this.TravelledDistance += distance;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Model} {this.FuelAmount:f2} {this.TravelledDistance}";
+    }
+}
---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/06.SpeedRacing/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var cars = GetCars();
+
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] commandArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            if (commandArgs[0] == "Drive")
+            {
+                string carModel = commandArgs[1];
+                double distance = double.Parse(commandArgs[2]);
+
+                cars.FirstOrDefault(c => c.Model == carModel).Drive(distance);
+            }
+        }
+
+        cars.ForEach(c => Console.WriteLine(c.ToString()));
+    }
+
+    private static List<Car> GetCars()
+    {
+        var cars = new List<Car>();
+
+        int carsCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < carsCount; i++)
+        {
+            string[] carArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string model = carArgs[0];
+            double fuelAmount = double.Parse(carArgs[1]);
+            double fuelConsumptionPerKilometer = double.Parse(carArgs[2]);
+
+            cars.Add(new Car(model, fuelAmount, fuelConsumptionPerKilometer));
+        }
+
+        return cars;
+    }
+}

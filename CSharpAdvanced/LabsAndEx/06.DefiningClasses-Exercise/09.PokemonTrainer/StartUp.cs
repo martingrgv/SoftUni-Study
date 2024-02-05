2e466b3bffaf007d9843e5b743afda66313c7bef
Martin <martin.geor04@gmail.com>
2024-02-05 19:48:57 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        List<Trainer> trainers = new List<Trainer>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "Tournament") // Add trainers
+        {
+            string[] inArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Trainer trainer = new Trainer(inArgs[0]);
+            Pokemon pokemon = new Pokemon(inArgs[1], inArgs[2], int.Parse(inArgs[3]));
+            trainer.AddPokemon(pokemon);
+
+            if (trainers.Any(t => t.Name == trainer.Name))
+            {
+                trainer = trainers.FirstOrDefault(t => t.Name == trainer.Name);
+                trainer.AddPokemon(pokemon);
+
+            }
+            else
+            {
+                trainers.Add(trainer);
+            }
+        }
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string element = input;
+
+            foreach (var trainer in trainers)
+            {
+                if (trainer.HasPokemonOfElement(element))
+                {
+                    trainer.IncreaseBadges();
+                }
+                else
+                {
+                    trainer.DamageAllPokemons();
+                    trainer.RemoveDeadPokemons();
+                }
+            }
+        }
+
+        List<Trainer> orderedTrainers = trainers.OrderByDescending(t => t.Badges).ToList();
+        orderedTrainers.ForEach(t => Console.WriteLine($"{t.Name} {t.Badges} {t.Pokemons.Count}"));
+    }
+}   
---
+++ CSharpAdvanced/LabsAndEx/06.DefiningClasses-Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,30 @@
+﻿namespace DefiningClasses;
+
+public class Trainer
+{
+    public Trainer()
+    {
+        this.Pokemons = new List<Pokemon>();
+        this.Badges = 0;
+    }
+
+    public Trainer(string name) : this()
+    {
+        this.Name = name;
+    }
+
+    public string Name { get; set; }
+    public int Badges { get; set; }
+    public List<Pokemon> Pokemons { get; set; }
+
+    public bool HasPokemonOfElement(string element) => this.Pokemons.Any(p => p.Element == element);
+
+    public void AddPokemon(Pokemon pokemon) => this.Pokemons.Add(pokemon);
+
+    public void IncreaseBadges() => this.Badges++;
+
+    public void DamageAllPokemons() => this.Pokemons.ForEach(p => p.Health -= 10);
+
+    public void RemoveDeadPokemons() => this.Pokemons.RemoveAll(p => p.Health <= 0);
+
+}

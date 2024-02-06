d2d8fecb6158a400d4d14f3fb95ef378634749fa
Martin <martin.geor04@gmail.com>
2024-02-06 22:03:09 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/09.Generics-Lab/BoxOfT/Box.cs
@@ -0,0 +1,18 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> box;
+
+        public Box()
+        {
+            this.box = new Stack<T>();
+        }
+
+        public int Count { get => this.box.Count; }
+
+        public void Add(T element) => box.Push(element);
+
+        public T Remove() => box.Pop();
+    }
+}

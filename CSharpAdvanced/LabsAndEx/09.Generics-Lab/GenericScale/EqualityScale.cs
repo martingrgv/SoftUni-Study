34c8026e351bca5e183dbb67efbd0caa41dd2974
Martin <martin.geor04@gmail.com>
2024-02-06 22:03:21 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/09.Generics-Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T1, T2>
+    {
+        private T1 element1;
+        private T2 element2;
+
+        public EqualityScale(T1 element1, T2 element2)
+        {
+            this.element1 = element1;
+            this.element2 = element2;
+        }
+
+        public bool AreEqual() => element1 == element2;
+    }
+}

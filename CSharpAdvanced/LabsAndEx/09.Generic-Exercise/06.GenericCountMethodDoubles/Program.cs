d979145dd87dbcab8a4e2cfa5a4147a3e058530d
Martin <martin.geor04@gmail.com>
2024-02-07 20:39:22 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/09.Generic-Exercise/06.GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,22 @@
+﻿using GenericBox;
+
+namespace _06.GenericCountMethodStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var box = new Box<double>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                box.Add(double.Parse(Console.ReadLine()));
+            }
+
+            double compareElement = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(box.CountGreaterThan(compareElement));
+        }
+    }
+}

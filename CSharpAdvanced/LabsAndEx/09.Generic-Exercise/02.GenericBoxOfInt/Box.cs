6aac8e6439bcd16165dccc028fcaa04fe16d0592
Martin <martin.geor04@gmail.com>
2024-02-07 16:09:48 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/09.Generic-Exercise/02.GenericBoxOfInt/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace GenericBoxOfInt
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.list.Add(item);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in list)
+                sb.AppendLine($"{typeof(T)}: {item}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharpAdvanced/LabsAndEx/09.Generic-Exercise/02.GenericBoxOfInt/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxOfInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var box = new Box<int>();
+
+            for (int i = 0; i < lines; i++)
+                box.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}

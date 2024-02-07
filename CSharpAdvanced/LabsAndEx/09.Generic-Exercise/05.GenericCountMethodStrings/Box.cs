34fe5dbcb7e81497f820f54662312f33a636f73a
Martin <martin.geor04@gmail.com>
2024-02-07 16:10:09 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/09.Generic-Exercise/05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace GenericBox
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.list.Add(item);
+        }
+
+        public int CountGreaterThan(T element)
+        {
+            int count = 0;
+
+            foreach (var item in list)
+            {
+                if (item.CompareTo(element) > 0)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in list)
+                sb.AppendLine($"{typeof(T)}: {item}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharpAdvanced/LabsAndEx/09.Generic-Exercise/05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using GenericBox;
+
+namespace _05.GenericCountMethodStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var box = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+
+            string compareElement = Console.ReadLine();
+
+            Console.WriteLine(box.CountGreaterThan(compareElement));
+        }
+    }
+}

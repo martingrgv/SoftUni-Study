fd2c98d0c0c61e41dfe25a8bc4c1aa496ae62874
Martin <martin.geor04@gmail.com>
2024-02-07 16:08:52 +0200

    Compare Ex.

---
+++ CSharpAdvanced/LabsAndEx/09.Generic-Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var box = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+                box.Add(Console.ReadLine());
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}

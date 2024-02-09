9cd0a8a3f08899bace03b875e74da03c448b58bc
Martin <martin.geor04@gmail.com>
2024-02-09 17:59:37 +0200

    Explaininig Params

---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Lab/Params/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Params
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNames(new string[] { "String", "Arr" });
+            Console.WriteLine();
+            PrintNames("Sending", "params", "in", "method");
+        }
+
+        // params keywords allows passing multiple arguments and stores them into an array
+        private static void PrintNames(params string[] arr)
+        {
+            foreach (var str in arr)
+                Console.Write(str + " ");
+        }
+    }
+}

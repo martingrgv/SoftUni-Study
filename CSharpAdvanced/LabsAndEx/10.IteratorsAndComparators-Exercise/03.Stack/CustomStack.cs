f6394b7a58de6e074988a8b4a8b773c358d6d564
martingrgv <martin.geor04@gmail.com>
2024-02-10 15:51:52 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace _03.Stack;
+public class CustomStack<T> : IEnumerable<T>
+{
+	private List<T> list;
+
+	public CustomStack()
+	{
+		this.list = new List<T>();
+	}
+
+	public void Push(params T[] values)
+	{
+		if (values.Length <= 0)
+			throw new InvalidOperationException();
+
+		foreach (var value in values)
+		{
+			this.list.Add(value);
+		}
+	}
+
+	public T Pop()
+	{
+		if (this.list.Count <= 0)
+			throw new InvalidOperationException();
+
+		int lastElementIndex = this.list.Count - 1;
+
+		T element = this.list[lastElementIndex];
+		this.list.RemoveAt(lastElementIndex);
+
+		return element;
+	}
+
+    public IEnumerator<T> GetEnumerator()
+    {
+		List<T> orderedStack = new List<T>(list);
+		orderedStack.Reverse();
+
+		foreach (var item in orderedStack)
+		{
+			yield return item;
+		}
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _03.Stack;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomStack<int> customStack = new CustomStack<int>();
+
+        string command;
+        while ((command = Console.ReadLine()) != "END")
+        {
+            string[] commandArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            switch(commandArgs[0])
+            {
+                case "Push":
+                    int[] pushElements = command
+                                        .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                        .Skip(1)
+                                        .Select(int.Parse)
+                                        .ToArray();
+
+                    customStack.Push(pushElements);
+                    break;
+                case "Pop":
+                    try
+                    {
+                        customStack.Pop();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                    break;
+            }
+        }
+
+        PrintTwice(customStack);
+    }
+
+    private static void PrintTwice<T>(CustomStack<T> stack)
+    {
+        for (int i = 0; i < 2; i++)
+            foreach (var item in stack)
+                Console.WriteLine(item);
+    }
+}

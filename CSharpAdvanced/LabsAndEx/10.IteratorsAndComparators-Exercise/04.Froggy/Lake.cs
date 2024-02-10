ec8fa52290492493ff774137c6b7888764f50547
martingrgv <martin.geor04@gmail.com>
2024-02-10 18:32:46 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace _04.Froggy;
+
+public class Lake : IEnumerable<int>
+{
+    private List<int> list;
+
+    public Lake(int[] stones)
+    {
+        this.list = new List<int>(stones);
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (int i = 0; i < this.list.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                yield return this.list[i];
+            }
+        }
+
+        for (int i = this.list.Count - 1; i >= 0; i--)
+        {
+            if (i % 2 != 0)
+            {
+                yield return this.list[i];
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.Froggy;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] stones = GetStones();
+        Lake lake = new Lake(stones);
+
+        Console.WriteLine(string.Join(", ", lake));
+    }
+
+    private static int[] GetStones() => Console.ReadLine()
+                                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse)
+                                        .ToArray();
+}

0346d553d81c00807f171d52eaa3fd0b976298d9
martingrgv <martin.geor04@gmail.com>
2024-02-12 16:46:57 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Exercise/07.CustomComparator/CustomComparator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _07.CustomComparator
+{
+    public class CustomComparator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+                return -1;
+            else if (x % 2 != 0 && y % 2 == 0)
+                return 1;
+            else
+                return x.CompareTo(y);
+        }
+    }
+}
+
---
+++ CSharpAdvanced/LabsAndEx/10.IteratorsAndComparators-Exercise/07.CustomComparator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.CustomComparator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+        Array.Sort(numbers, new CustomComparator());
+
+        Console.WriteLine(string.Join(' ', numbers));
+    }
+}
+

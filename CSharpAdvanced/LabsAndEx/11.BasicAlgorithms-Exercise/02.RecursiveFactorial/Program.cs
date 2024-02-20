318ced43c83aa5da56c800b5ffef3d7c0e9ee93b
Martin <martin.geor04@gmail.com>
2024-02-20 21:45:55 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/11.BasicAlgorithms-Exercise/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorial(n));
+        }
+
+        private static long GetFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * GetFactorial(--n);
+        }
+    }
+}

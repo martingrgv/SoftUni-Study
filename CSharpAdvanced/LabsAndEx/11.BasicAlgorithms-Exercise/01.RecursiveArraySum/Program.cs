ca3e8e19eb35c71eec88fac61e4e380e570cc507
Martin <martin.geor04@gmail.com>
2024-02-20 21:45:42 +0200

    Completed Ex.

---
+++ CSharpAdvanced/LabsAndEx/11.BasicAlgorithms-Exercise/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            Console.WriteLine(SumNumbers(numbers));
+        }
+
+        private static int SumNumbers(int[] numbers, int index = 0)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + SumNumbers(numbers, ++index);
+        }
+    }
+}

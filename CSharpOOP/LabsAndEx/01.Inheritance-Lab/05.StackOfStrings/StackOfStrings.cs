7ddfed270e8e255c56d7fe06dfe1e6e41835d34a
Martin <martin.geor04@gmail.com>
2024-02-22 00:30:06 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/01.Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<String>
+    {
+        public bool IsEmpty() => Count == 0;
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var element in collection)
+                Push(element);
+        }
+    }
+}
---
+++ CSharpOOP/LabsAndEx/01.Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stringStack = new StackOfStrings();
+
+            Console.WriteLine(stringStack.IsEmpty());
+
+            stringStack.AddRange(new string[] { "element1", "element2"});
+            Console.WriteLine(string.Join(' ', stringStack));
+        }
+    }
+}

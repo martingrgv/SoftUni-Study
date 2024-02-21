c1ddaec24905209d4f98e76595f2730d0f920dea
Martin <martin.geor04@gmail.com>
2024-02-22 00:21:49 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/01.Inheritance-Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, Count - 1);
+
+            string removedElement = this[index];
+            RemoveAt(index);
+
+            return removedElement;
+        }
+    }
+}
---
+++ CSharpOOP/LabsAndEx/01.Inheritance-Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rndList = new RandomList();
+
+            rndList.Add("str1");
+            rndList.Add("str2");
+            rndList.Add("str3");
+
+            Console.WriteLine(string.Join(' ', rndList));
+
+            Console.WriteLine(rndList.RandomString());
+
+            Console.WriteLine(string.Join(' ', rndList));
+        }
+    }
+}

fdf6ae25fe4556a4e97339aed0ed815845ef79ab
Martin <martin.geor04@gmail.com>
2024-02-22 23:18:13 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/01.Inheritance-Exercise/Restaurant/Cake.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double cakeGrams = 250;
+        private const decimal cakePrice = 5m;
+        private const double cakeCalories = 1000;
+
+        public Cake(string name) : base(name, cakePrice, cakeGrams, cakeCalories)
+        {
+        }
+    }
+}

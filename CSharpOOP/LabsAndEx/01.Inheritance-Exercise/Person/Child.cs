c0ae363d9c1a879d2fd28621c68acfd0adfed2f5
Martin <martin.geor04@gmail.com>
2024-02-22 23:26:00 +0200

    Updated project

--- CSharpOOP/LabsAndEx/01.Inheritance-Exercise/Person/Child.cs
+++ CSharpOOP/LabsAndEx/01.Inheritance-Exercise/Person/Child.cs
@@ -1,10 +1,26 @@
-﻿namespace Person
+﻿using System;
+
+namespace Person
 {
     public class Child : Person
     {
+        private int age;
+
         public Child(string name, int age) : base(name, age)
         {
-            
+            Age = age; 
+        }
+
+        public new int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 15)
+                    throw new ArgumentException("Child cannot be over 15 years of old.");
+                else
+                    age = value;
+            }
         }
     }
 }
--- CSharpOOP/LabsAndEx/01.Inheritance-Exercise/Person/Person.cs
+++ CSharpOOP/LabsAndEx/01.Inheritance-Exercise/Person/Person.cs
@@ -1,7 +1,11 @@
-﻿namespace Person
+﻿using System;
+
+namespace Person
 {
     public class Person
     {
+        private int age;
+
         public Person(string name, int age)
         {
             Name = name; 
@@ -9,7 +13,17 @@
         }
 
         public string Name { get; set; }
-        public int Age { get; set; }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Age cannot be a negative number.");
+                else
+                    age = value;
+            }
+        }
 
         public override string ToString()
         {

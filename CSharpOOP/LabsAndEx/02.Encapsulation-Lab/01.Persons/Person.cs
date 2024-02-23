c7b4aa587cb0cc5f85e7f85f89b5d6c237314655
martingrgv <martin.geor04@gmail.com>
2024-02-24 01:08:38 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/02.Encapsulation-Lab/01.Persons/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonsInfo;
+
+public class Person
+{
+	private int age;
+
+	public Person(string firstName, string lastName, int age)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		Age = age;
+	}
+
+	public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+	public int Age
+	{
+		get { return age; }
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Age cannot be a negative number.");
+			}
+
+			age = value;
+		}
+	}
+
+    public override string ToString()
+    {
+		return $"{FirstName} {LastName} is {Age} years old.";
+    }
+}

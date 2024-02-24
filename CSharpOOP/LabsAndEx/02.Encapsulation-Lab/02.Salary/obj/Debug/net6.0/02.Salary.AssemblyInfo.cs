39aeb36a7a7313cc74d65416a89dd52a76531677
Martin <martin.geor04@gmail.com>
2024-02-24 15:49:21 +0200

    #

--- CSharpOOP/LabsAndEx/02.Encapsulation-Lab/02.Salary/obj/Debug/net6.0/02.Salary.AssemblyInfo.cs
+++ CSharpOOP/LabsAndEx/02.Encapsulation-Lab/02.Salary/obj/Debug/net6.0/02.Salary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02.Salary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70158c70cb0d8e0c2b5f373916e59281fe0df99f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b12a4e3fcf0b6d3a6a6467a21dff661de0721d")]
 [assembly: System.Reflection.AssemblyProductAttribute("02.Salary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02.Salary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

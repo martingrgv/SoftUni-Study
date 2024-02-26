2cd66e606fff711abf0c0fb375b29cad2798e945
Martin <martin.geor04@gmail.com>
2024-02-26 22:41:32 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/02.Encapsulation-Exercise/03.ShoppingSpree/Models/Person.cs
@@ -0,0 +1,66 @@
+﻿namespace ShoppingSpree.Models
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public string Name 
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        private IReadOnlyCollection<Product> ProductBag
+        {
+            get { return products.AsReadOnly(); }
+        }
+
+        public void AddProductToBag(Product product)
+        {
+            products.Add(product);
+        }
+
+        public override string ToString()
+        {
+            if (products.Count > 0)
+            {
+                return $"{Name} - {string.Join(", ", products)}";
+            }
+            else
+            {
+                return $"{Name} - Nothing bought";
+            }
+        }
+    }
+}
---
+++ CSharpOOP/LabsAndEx/02.Encapsulation-Exercise/03.ShoppingSpree/StartUp.cs
@@ -0,0 +1,90 @@
+﻿using ShoppingSpree.Models;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Person> people = GetPeople();
+                List<Product> products = GetProducts();
+
+                string input;
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] data = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    string personName = data[0];
+                    string productName = data[1];
+
+                    Person currentPerson = people.FirstOrDefault(p => p.Name == personName);
+                    Product currentProduct = products.FirstOrDefault(p => p.Name == productName);
+
+                    if (currentPerson != null && currentProduct != null)
+                    {
+                        if (currentPerson.Money - currentProduct.Cost >= 0)
+                        {
+                            currentPerson.AddProductToBag(currentProduct);
+                            currentPerson.Money -= currentProduct.Cost;
+
+                            Console.WriteLine($"{currentPerson.Name} bought {currentProduct.Name}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{currentPerson.Name} can't afford {currentProduct.Name}");
+                        }
+                    }
+                }
+
+                foreach (var person in people)
+                {
+                    Console.WriteLine(person);
+                }
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static List<Person> GetPeople()
+        {
+            List<Person> people = new List<Person>();
+            string[] peopleInput = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < peopleInput.Length; i++)
+            {
+                people.Add(GetPerson(peopleInput[i]));
+            }
+
+            return people;
+        }
+
+        private static Person GetPerson(string personInput)
+        {
+            string[] personArgs = personInput.Split('=', StringSplitOptions.RemoveEmptyEntries);
+            return new Person(personArgs[0], decimal.Parse(personArgs[1]));
+        }
+
+        private static List<Product> GetProducts()
+        {
+
+            List<Product> products = new List<Product>();
+            string[] productsInput = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < productsInput.Length; i++)
+            {
+                products.Add(GetProduct(productsInput[i]));
+            }
+
+            return products;
+        }
+
+        private static Product GetProduct(string productInput)
+        {
+            string[] productArgs = productInput.Split('=', StringSplitOptions.RemoveEmptyEntries);
+            return new Product(productArgs[0], decimal.Parse(productArgs[1]));
+        }
+    }
+}

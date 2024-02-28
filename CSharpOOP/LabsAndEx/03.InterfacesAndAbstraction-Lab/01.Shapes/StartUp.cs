490f37aef4373d9b2809d4b3126d3d23d7b83858
martingrgv <martin.geor04@gmail.com>
2024-02-28 15:56:43 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/03.InterfacesAndAbstraction-Lab/01.Shapes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Shapes;
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Func<int> GetInput = () => int.Parse(Console.ReadLine());
+
+        IDrawable circle = new Circle(GetInput());
+        IDrawable rect = new Rectangle(GetInput(), GetInput());
+
+        circle.Draw();
+        rect.Draw();
+    }
+}
+

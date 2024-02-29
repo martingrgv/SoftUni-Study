1b0d3d2c6bfb8e99f6c1148084cec6044ae3045f
martingrgv <martin.geor04@gmail.com>
2024-02-29 21:13:04 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/03.InterfacesAndAbstraction-Exercise/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,67 @@
+﻿namespace PersonInfo;
+
+public class Citizen : IPerson, IIdentifiable, IBirthable
+{
+	private int age;
+	private string id;
+	private string birthdate;
+
+	public Citizen(string name, int age, string id, string birthdate)
+	{
+		Name = name;
+		Age = age;
+		Id = id;
+		Birthdate = birthdate;
+	}
+
+    public string Name { get; set; }
+
+    public int Age
+	{
+		get { return age; }
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Age cannot be a negative number.");
+			}
+
+			age = value;
+		}
+	}
+
+    public string Id
+	{
+		get { return id; }
+		private set
+		{
+			if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+			{
+				throw new ArgumentException("Id cannot be empty.");
+			}
+
+			id = value;
+		}
+	}
+
+    public string Birthdate
+	{
+		get { return birthdate; }
+		private set
+		{
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Id cannot be empty.");
+            }
+
+			birthdate = value;
+        }
+	}
+
+    public override string ToString()
+    {
+		return Id
+            + Environment.NewLine
+			+ Birthdate;
+    }
+}
---
+++ CSharpOOP/LabsAndEx/03.InterfacesAndAbstraction-Exercise/02.MultipleImplementation/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonInfo;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        IPerson person = new Citizen(Console.ReadLine(), int.Parse(Console.ReadLine()), Console.ReadLine(), Console.ReadLine());
+        Console.WriteLine(person);
+    }
+}
+

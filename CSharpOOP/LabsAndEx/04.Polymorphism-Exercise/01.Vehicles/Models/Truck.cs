f0aebdc464f8f06ff4d4a9a1c248430308fb7831
Martin <martin.geor04@gmail.com>
2024-03-07 04:18:04 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/04.Polymorphism-Exercise/01.Vehicles/Models/Truck.cs
@@ -0,0 +1,17 @@
+﻿namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double fuelConsumptionModifier = 1.6;
+
+        public Truck(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption + fuelConsumptionModifier)
+        {
+
+        }
+
+        public override void Refuel(double fuelAmount)
+        {
+            base.Refuel(fuelAmount * 0.95);
+        }
+    }
+}
---
+++ CSharpOOP/LabsAndEx/04.Polymorphism-Exercise/01.Vehicles/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using Vehicles.Models;
+
+namespace Vehicles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Car car = new Car(double.Parse(carData[1]), double.Parse(carData[2]));
+
+            string[] truckData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Truck truck = new Truck(double.Parse(truckData[1]), double.Parse(truckData[2]));
+
+            int commandsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    switch (command[0])
+                    {
+                        case "Drive":
+                            if (command[1] == "Car") car.Drive(double.Parse(command[2]));
+                            else if (command[1] == "Truck") truck.Drive(double.Parse(command[2]));
+                            break;
+                        case "Refuel":
+                            if (command[1] == "Car") car.Refuel(double.Parse(command[2]));
+                            else if (command[1] == "Truck") truck.Refuel(double.Parse(command[2]));
+                            break;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine($"Car: {car.FuelQuantity:f2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:f2}");
+        }
+    }
+}

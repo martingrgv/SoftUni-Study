9de071081b3346d7ff171824a0bedc0550ed599f
Martin <martin.geor04@gmail.com>
2024-03-08 13:27:04 +0200

    Added project

---
+++ CSharpOOP/LabsAndEx/04.Polymorphism-Exercise/02.VehicleExtension/Models/Vehicle.cs
@@ -0,0 +1,67 @@
+﻿using Vehicles.Interfaces;
+
+namespace Vehicles.Models
+{
+    public abstract class Vehicle : IDrivable, IRefuelable
+    {
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        protected Vehicle(double fuelQuantity, double fuelConsumption)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public double FuelQuantity 
+        { 
+            get { return fuelQuantity; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Fuel quantity cannot be negative!");
+                }
+
+                fuelQuantity = value;
+            }
+        }
+
+        public double FuelConsumption 
+        {
+            get { return fuelConsumption; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Fuel consumption cannot be negative or zero!");
+                }
+
+                fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            double totalConsumption = FuelConsumption * distance;
+            if (totalConsumption > FuelQuantity)
+            {
+                throw new ArgumentException($"{GetType().Name} needs refueling");
+            }
+
+            FuelQuantity -= totalConsumption;
+            Console.WriteLine($"{GetType().Name} travelled {distance} km");
+        }
+
+
+        public virtual void Refuel(double fuelAmount)
+        {
+            if (fuelAmount <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+
+            FuelQuantity += fuelAmount;
+        }
+    }
+}

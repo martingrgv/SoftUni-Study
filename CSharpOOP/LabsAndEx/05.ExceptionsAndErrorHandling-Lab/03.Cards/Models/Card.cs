616d9a62f8144ffd6625b1cb7bc5430c2efe42de
martingrgv <martin.geor04@gmail.com>
2024-03-11 21:38:25 +0200

    Completed Ex.

--- CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/03.Cards/Models/Card.cs
+++ CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/03.Cards/Models/Card.cs
@@ -1,6 +1,60 @@
-namespace _03.Cards.Models;
+namespace Cards.Models;
 
 public class Card
 {
+    private readonly string[] validFaces = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private readonly string[] validSuits = { "H", "C", "D", "S"};
+    private string face;
+    private string suit;
+
+    public Card(string face, string suit)
+    {
+        Face = face;
+        Suit = suit;
+    }
+
+    public string Face
+    {
+        get { return face; }
+        private set
+        {
+            if (!validFaces.Contains(value))
+            {
+                throw new ArgumentException("Invalid card!");
+            }
+
+            face = value;
+        }
+    }
     
+    public string Suit
+    {
+        get { return suit; }
+        private set
+        {
+            if (!validSuits.Contains(value))
+            {
+                throw new ArgumentException("Invalid card!");
+            }
+
+            suit = value;
+        }
+    }
+
+    private string SuitSymbol
+    {
+        get
+        {
+            if (Suit == "H") return "\u2665";
+            else if (Suit == "C") return "\u2663";
+            else if (Suit == "D") return "\u2666";
+            else if (Suit == "S") return "\u2660";
+            else throw new ArgumentException("Card suit cannot be null");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"[{Face}{SuitSymbol}]";
+    }
 }
---
+++ CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/03.Cards/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using Cards.Models;
+
+namespace Cards;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        List<Card> cards = new List<Card>();
+
+        string[] cardsData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < cardsData.Length; i++)
+        {
+            string[] cardData = cardsData[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                cards.Add(new Card(cardData[0], cardData[1]));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        foreach (var card in cards)
+            Console.Write(card + " ");
+    }
+}

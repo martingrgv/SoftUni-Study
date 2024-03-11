0872574defc93c6cb2cbbb6dcaad20aed4ea5dba
martingrgv <martin.geor04@gmail.com>
2024-03-11 21:37:39 +0200

    Completed Ex.

---
+++ CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/01.SquareRoot/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace ExceptionHandling;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int number = GetNumber();
+            Console.WriteLine(Math.Sqrt(number));
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye.");
+        }
+    }
+
+    private static int GetNumber()
+    {
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+        return 0;
+    }
+}
---
+++ CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/03.Cards/Models/Card.cs
@@ -0,0 +1,6 @@
+namespace _03.Cards.Models;
+
+public class Card
+{
+    
+}

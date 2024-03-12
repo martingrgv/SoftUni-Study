d42293d8692ad87865706d99a1de995cc297d8d4
martingrgv <martin.geor04@gmail.com>
2024-03-12 19:45:59 +0200

    Added project

---
+++ CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/02.EnterNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExceptionHandling;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine(string.Join(", ", ReadNumbers(1, 100)));
+    }
+
+    //private static int[] ReadNumbers(int start, int end)
+    //{
+    //}
+}

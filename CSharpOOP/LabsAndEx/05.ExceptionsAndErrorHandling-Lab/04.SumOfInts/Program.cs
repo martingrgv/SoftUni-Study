03c022e35b0bdc98b0f4a00cd4f9aa57d9eb2536
martingrgv <martin.geor04@gmail.com>
2024-03-13 19:35:10 +0200

    Added project

---
+++ CSharpOOP/LabsAndEx/05.ExceptionsAndErrorHandling-Lab/04.SumOfInts/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SumOfInts;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0;
+        string[] data = Console.ReadLine()
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            try
+            {
+                sum += int.Parse(data[i]);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"The element '{data[i]}' is out of range!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"The element '{data[i]}' is in wrong format!");
+            }
+            finally
+            {
+                Console.WriteLine($"Element '{data[i]}' processed - current sum: {sum}");
+            }
+        }
+
+        Console.WriteLine($"The total sum of all integers is: {sum}");
+    }
+}

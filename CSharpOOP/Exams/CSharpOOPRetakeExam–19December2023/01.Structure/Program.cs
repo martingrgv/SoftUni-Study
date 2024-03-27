12f0e79551102d9b640a3a2a4b9af18174b5bdcd
martingrgv <martin.geor04@gmail.com>
2024-03-27 23:41:03 +0200

    Added project

---
+++ CSharpOOP/Exams/CSharpOOPRetakeExam–19December2023/01.Structure/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.Structure;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ CSharpOOP/Exams/CSharpOOPRetakeExam–19December2023/HighwayToPeak-Skeleton/Models/Peak.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using HighwayToPeak.Models.Contracts;
+
+namespace HighwayToPeak.Models
+{
+    public class Peak : IPeak
+	{
+        private string name;
+        private int elevation;
+        private string difficultyLevel;
+		public Peak()
+		{
+		}
+
+        public string Name
+        {
+            get { return name; }
+            set
+            { 
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Peak name cannot be null or whitespace.");
+		        }
+
+                name = value;
+	        }
+	    }   
+
+        public int Elevation
+        { 
+            get { return elevation; }
+            set
+            { 
+                // Could be more than 0 only
+                if (value < 0)
+                {
+                    throw new ArgumentException("Peak elevation must be a positive value.");
+		        }
+
+                elevation = value;
+	        }
+	    }
+
+        public string DifficultyLevel
+        { 
+            get { return difficultyLevel; }
+	    }
+
+        public override string ToString()
+        {
+            return $"Peak: {Name} -> Elevation: {Elevation}, Difficulty: {DifficultyLevel}";
+        }
+    }
+}
+

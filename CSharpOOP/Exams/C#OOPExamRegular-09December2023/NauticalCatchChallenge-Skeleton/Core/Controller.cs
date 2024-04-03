d91dd7d35278ab10466da58955287ec151d0f65e
martingrgv <martin.geor04@gmail.com>
2024-04-03 20:05:52 +0300

    Create Controller.cs

---
+++ CSharpOOP/Exams/C#OOPExamRegular-09December2023/NauticalCatchChallenge-Skeleton/Core/Controller.cs
@@ -0,0 +1,36 @@
+using NauticalCatchChallenge.Core.Contracts;
+
+namespace NauticalCatchChallenge.Core;
+
+public class Controller : IController
+{
+    public string DiveIntoCompetition(string diverType, string diverName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string SwimIntoCompetition(string fishType, string fishName, double points)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ChaseFish(string diverName, string fishName, bool isLucky)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string HealthRecovery()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string DiverCatchReport(string diverName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string CompetitionStatistics()
+    {
+        throw new NotImplementedException();
+    }
+}

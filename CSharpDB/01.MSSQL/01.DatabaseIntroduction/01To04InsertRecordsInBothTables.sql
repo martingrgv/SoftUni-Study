72e1960afc648b4f757fd9402f5408d12ff414d0
martingrgv <martin.geor04@gmail.com>
2024-05-14 18:08:05 +0300

    Created db course files

---
+++ CSharpDB/01.MSSQL/01.DatabaseIntroduction/01To04InsertRecordsInBothTables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Minions(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(20) NOT NULL,
+	Age INT,
+	TownId INT
+)
+
+CREATE TABLE Towns(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(50)
+)
+
+ALTER TABLE Minions
+ADD CONSTRAINT FK_TownID 
+FOREIGN KEY (TownId) REFERENCES Towns(Id)
+
+INSERT INTO Towns(Id, [Name])
+VALUES (1, 'Sofia'),
+	(2, 'Plovdiv'),
+	(3, 'Varna')
+
+INSERT INTO Minions(Id, [Name], Age, TownId)
+VALUES (1, 'Kevin', 22, 1),
+	  (2, 'Bob', 15, 3),
+	  (3, 'Steward', NULL, 2)

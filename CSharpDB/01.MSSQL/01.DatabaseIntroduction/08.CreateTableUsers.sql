d4d415cbb0fb691a8f664d9df6db5da8f04d6881
martingrgv <martin.geor04@gmail.com>
2024-05-16 21:16:36 +0300

    Update 08.CreateTableUsers.sql

--- CSharpDB/01.MSSQL/01.DatabaseIntroduction/08.CreateTableUsers.sql
+++ CSharpDB/01.MSSQL/01.DatabaseIntroduction/08.CreateTableUsers.sql
@@ -1,8 +1,8 @@
 -- 08.CREATE TABLE USERS
 
 CREATE TABLE Users(
-	Id INT PRIMARY KEY IDENTITY(1, 1),
-	Username VARCHAR(30) UNIQUE NOT NULL,
+	Id BIGINT PRIMARY KEY IDENTITY(1, 1),
+	Username VARCHAR(30) NOT NULL,
 	[Password] VARCHAR(26) NOT NULL,
 	ProfilePicture VARBINARY(MAX),
 	LastLoginTime TIME,
@@ -20,4 +20,41 @@ VALUES ('The Witch of Hemwick', 'TheKeyToRunes', '00:00:00', 0),
 
 SELECT * FROM Users
 
-DROP TABLE Users
+-- 09.
+-- You will have to change the PK name
+ALTER TABLE Users
+DROP CONSTRAINT PK__Users__3214EC07F937C434
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_UsersTable PRIMARY KEY (Id, Username)
+
+-- 10.
+ALTER TABLE Users
+ADD CONSTRAINT CHK_PasswordIsAtLeastFiveSymbols
+CHECK (LEN([Password]) >= 5)
+
+-- 11.
+ALTER TABLE Users
+ADD CONSTRAINT df_LastLoginTime
+DEFAULT GETDATE() FOR LastLoginTime;
+
+-- 12.
+ALTER TABLE Users
+DROP CONSTRAINT PK_UsersTable
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_Users PRIMARY KEY (Id)
+
+ALTER TABLE Users
+ADD UNIQUE (Username)
+
+ALTER TABLE Users
+ADD CONSTRAINT CHK_UsernameAtLeastThreeSymbols
+CHECK (LEN(Username) >= 3)
+
+-- Test
+
+INSERT INTO Users(Username, [Password])
+VALUES ('One Reborn', 'Yahargul Chapel')
+
+--DROP TABLE Users

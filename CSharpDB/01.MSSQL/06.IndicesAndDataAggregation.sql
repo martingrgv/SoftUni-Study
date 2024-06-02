aba46cde9b773765920abbc6b39c5cf8e4f0e6e4
martingrgv <martin.geor04@gmail.com>
2024-06-02 15:09:35 +0300

    Completed exercises

--- CSharpDB/01.MSSQL/06.IndicesAndDataAggregation.sql
+++ CSharpDB/01.MSSQL/06.IndicesAndDataAggregation.sql
@@ -50,3 +50,63 @@ HAVING SUM(DepositAmount) < 150000
 ORDER BY TotalSum DESC
 
 --08.
+SELECT 
+	DepositGroup
+	,MagicWandCreator
+	,MIN(DepositCharge) AS MinDepositCharge
+FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+ORDER BY MagicWandCreator, DepositGroup ASC
+
+--09.
+SELECT AgeGroup, COUNT(*) AS Wizards
+FROM
+(
+	SELECT 
+		CASE
+			WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+			WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+			WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+			WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+			WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+			WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+			WHEN Age >= 61 THEN '[61+]'
+		END AS AgeGroup
+	FROM WizzardDeposits
+) AS AgeGroups
+GROUP BY AgeGroup
+
+--10.
+SELECT FirstLetter
+FROM
+(
+	SELECT DISTINCT
+		LEFT(FirstName, 1) AS FirstLetter
+	FROM WizzardDeposits
+	WHERE DepositGroup = 'Troll Chest'
+	GROUP BY FirstName
+) AS WizardsTrollChestDeposi
+ORDER BY FirstLetter ASC
+
+--11.
+SELECT
+	DepositGroup
+	,IsDepositExpired
+	,AVG(DepositInterest) AS AverageInterest
+FROM WizzardDeposits
+WHERE DepositStartDate >= '01.01.1985'
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired ASC
+
+--12.
+SELECT SUM([Difference]) AS SumDifference
+FROM
+(
+SELECT 
+	FirstName AS HostWizzard,
+	DepositAmount AS HostWizzardDeposit,
+	LEAD(FirstName) OVER (ORDER BY Id) AS GuestWizzard,
+	LEAD([DepositAmount]) OVER (ORDER BY Id) AS GuestWizzardDeposit,
+	(DepositAmount - LEAD([DepositAmount]) OVER (ORDER BY Id)) AS [Difference]
+FROM WizzardDeposits
+) AS SubQuery

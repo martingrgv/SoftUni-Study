bab0682c60e3fe1902804e451cdb3e9c59faa30c
martingrgv <martin.geor04@gmail.com>
2024-06-02 14:40:20 +0300

    Completed exercises

---
+++ CSharpDB/01.MSSQL/06.IndicesAndDataAggregation.sql
@@ -0,0 +1,52 @@
+SELECT * FROM WizzardDeposits
+
+--01.
+SELECT
+	COUNT(*) AS [Count]
+FROM WizzardDeposits
+
+--02.
+SELECT
+	MAX(MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits
+
+--03.
+SELECT
+	DepositGroup,
+	MAX(MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits
+GROUP BY DepositGroup
+
+--04.
+SELECT TOP 2
+	DepositGroup
+FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY AVG(MagicWandSize) ASC
+
+--05.
+SELECT
+	DepositGroup
+	,SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup
+
+--06.
+SELECT
+	DepositGroup
+	,SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+
+--07.
+SELECT
+	DepositGroup
+	,SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
+--08.

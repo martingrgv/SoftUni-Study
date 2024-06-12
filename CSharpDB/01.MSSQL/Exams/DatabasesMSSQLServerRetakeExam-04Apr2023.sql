c3de99c70da33cb026bc753c8e6a0dfec985139e
martingrgv <martin.geor04@gmail.com>
2024-06-12 13:33:09 +0300

    Completed exercises

--- CSharpDB/01.MSSQL/Exams/DatabasesMSSQLServerRetakeExam-04Apr2023.sql
+++ CSharpDB/01.MSSQL/Exams/DatabasesMSSQLServerRetakeExam-04Apr2023.sql
@@ -64,3 +64,42 @@ CREATE TABLE ProductsClients(
 )
 
 --02.
+INSERT INTO Products([Name], Price, CategoryId, VendorId) VALUES
+	('SCANIA Oil Filter XD01', 78.69, 1, 1),
+	('MAN Air Filter XD01',	97.38, 1, 5),
+	('DAF Light Bulb 05FG87', 55.00, 2, 13),
+	('ADR Shoes 47-47.5', 49.85, 3,	5),
+	('Anti-slip pads S', 5.87, 5, 7)
+
+INSERT INTO Invoices(Number, IssueDate, DueDate, Amount, Currency, ClientId) VALUES
+	(1219992181, '2023-03-01', '2023-04-30', 180.96, 'BGN',	3),
+	(1729252340, '2022-11-06', '2023-01-04', 158.18, 'EUR',	13),
+	(1950101013, '2023-02-17', '2023-04-18', 615.15, 'USD',	19)
+
+--03.
+UPDATE Invoices
+SET DueDate = '2023-04-01'
+WHERE DATEPART(MONTH, IssueDate) = 11 AND
+	DATEPART(YEAR, IssueDate) = 2022
+
+UPDATE Clients
+SET AddressId = 3
+WHERE [Name] LIKE '%CO%'
+
+--04.
+DECLARE @ClientsToDelete TABLE(Id INT)
+
+INSERT INTO @ClientsToDelete
+	SELECT Id FROM Clients
+	WHERE LEFT(NumberVAT, 2) = 'IT'
+
+DELETE FROM ProductsClients
+WHERE ClientId IN (SELECT Id FROM @ClientsToDelete)
+
+DELETE FROM Invoices
+WHERE ClientId IN (SELECT Id FROM @ClientsToDelete)
+
+DELETE FROM Clients
+WHERE Id IN (SELECT Id FROM @ClientsToDelete)
+
+--05.

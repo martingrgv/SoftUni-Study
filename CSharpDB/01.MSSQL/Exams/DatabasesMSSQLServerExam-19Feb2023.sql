9939faf1c65edb1be8050718037c965c488546c8
martingrgv <martin.geor04@gmail.com>
2024-06-15 17:20:22 +0300

    Completed ex

--- CSharpDB/01.MSSQL/Exams/DatabasesMSSQLServerExam-19Feb2023.sql
+++ CSharpDB/01.MSSQL/Exams/DatabasesMSSQLServerExam-19Feb2023.sql
@@ -195,3 +195,21 @@ BEGIN
 END
 
 --12.
+CREATE PROCEDURE usp_SearchByCategory(@category VARCHAR(50))
+AS
+BEGIN
+	SELECT
+		b.[Name],
+		b.YearPublished,
+		b.Rating,
+		c.[Name] AS CategoryName,
+		p.[Name] AS PublisherName,
+		CONCAT(pr.PlayersMin, ' ', 'people') AS MinPlayers,
+		CONCAT(pr.PlayersMax, ' ', 'people') AS MaxPlayers
+	FROM Boardgames AS b
+	JOIN Categories AS c ON b.CategoryId = c.Id
+	JOIN Publishers AS p ON b.PublisherId = p.Id
+	JOIN PlayersRanges AS pr ON b.PlayersRangeId = pr.Id
+	WHERE c.[Name] = @category
+	ORDER BY p.[Name], b.YearPublished DESC
+END

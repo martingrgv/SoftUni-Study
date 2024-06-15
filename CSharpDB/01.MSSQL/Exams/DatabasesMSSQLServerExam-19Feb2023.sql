95de26846f5801df12efb80ec5559a7bd5385df8
martingrgv <martin.geor04@gmail.com>
2024-06-15 16:30:36 +0300

    Completed ex

--- CSharpDB/01.MSSQL/Exams/DatabasesMSSQLServerExam-19Feb2023.sql
+++ CSharpDB/01.MSSQL/Exams/DatabasesMSSQLServerExam-19Feb2023.sql
@@ -153,3 +153,17 @@ WHERE (Rating > 7 AND
 	(pr.PlayersMin = 2 AND
 	pr.PlayersMax = 5))
 ORDER BY b.[Name], b.Rating DESC
+
+--09.
+SELECT 
+	CONCAT(c.FirstName, ' ', c.LastName) AS FullName,
+	c.Email,
+	MAX(b.Rating) AS Rating
+FROM CreatorsBoardgames AS cb
+JOIN Creators AS c ON cb.CreatorId = c.Id
+JOIN Boardgames AS b ON cb.BoardgameId = b.Id
+WHERE RIGHT(c.Email, 4) = '.com'
+GROUP BY c.FirstName, c.LastName, c.Email
+ORDER BY FullName
+
+--10.

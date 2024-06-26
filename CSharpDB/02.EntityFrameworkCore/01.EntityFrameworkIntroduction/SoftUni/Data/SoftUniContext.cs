aa7ce66aea6991a0986554a8d3d0292e7e00e7ed
martingrgv <martin.geor04@gmail.com>
2024-06-26 17:30:28 +0300

    Complted ex

--- CSharpDB/02.EntityFrameworkCore/01.EntityFrameworkIntroduction/SoftUni/Data/SoftUniContext.cs
+++ CSharpDB/02.EntityFrameworkCore/01.EntityFrameworkIntroduction/SoftUni/Data/SoftUniContext.cs
@@ -25,7 +25,7 @@ namespace SoftUni.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SoftUni;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;Integrated Security=True;");
             }
         }
 
--- CSharpDB/02.EntityFrameworkCore/01.EntityFrameworkIntroduction/SoftUni/StartUp.cs
+++ CSharpDB/02.EntityFrameworkCore/01.EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -1,10 +1,38 @@
-﻿namespace SoftUni
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            using (var context = new SoftUniContext())
+            {
+                Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            }
+        }
 
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            return string.Join(Environment.NewLine, context.Employees
+                .Select(e => $"{ e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                { 
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName);
+
+            return string.Join(Environment.NewLine, employees
+                .Select(e => $"{e.FirstName} - {e.Salary:f2}"));
         }
     }
 }

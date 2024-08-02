3c74113de27ecc419e5f86f466ada015b91fc59c
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-02 23:50:25 +0300

    Changed db connection string

--- CSharpDB/02.EntityFrameworkCore/Exams/C#DBAdvancedRetakeExam-11April2023/Invoices/Data/Configuration.cs
+++ CSharpDB/02.EntityFrameworkCore/Exams/C#DBAdvancedRetakeExam-11April2023/Invoices/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=.;Database=InvoicesExamDB;Integrated Security=True";
+        public static string ConnectionString = @"Server=localhost;Database=InvoiceDB;User Id=SA;Password=MasterDev1";
     }
 }

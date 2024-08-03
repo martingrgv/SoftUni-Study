59b4e58c21a26457ba303905329e46b3ac8117d4
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 10:28:08 +0300

    Added DbContext seed and seed migrations

--- CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/Migrations/20240803072725_DbSeed.Designer.cs
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/Migrations/20240803072725_DbSeed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelAgency.Data;
 
@@ -11,9 +12,10 @@ using TravelAgency.Data;
 namespace TravelAgency.Migrations
 {
     [DbContext(typeof(TravelAgencyContext))]
-    partial class TravelAgencyContextModelSnapshot : ModelSnapshot
+    [Migration("20240803072725_DbSeed")]
+    partial class DbSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

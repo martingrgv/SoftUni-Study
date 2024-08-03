b234bed7d4a485161155f590f7553b5973c080f6
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 12:29:16 +0300

    Created booking and customer export DTOs

---
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/ExportDtos/CustomerExportDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    public class CustomerExportDTO
+    {
+        [JsonProperty(nameof(FullName))]
+        public string FullName { get; set; } = null!;
+
+        [JsonProperty(nameof(PhoneNumber))]
+        public string PhoneNumber { get; set; } = null!;
+
+        [JsonProperty(nameof(Bookings))]
+        public BookingExportDTO[] Bookings { get; set; } = null!;
+    }
+}

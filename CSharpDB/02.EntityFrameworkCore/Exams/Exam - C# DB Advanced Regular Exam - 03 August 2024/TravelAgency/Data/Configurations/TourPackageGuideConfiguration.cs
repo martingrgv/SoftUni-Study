a95b1fb83edad0f203ebff2ac9c523e1a4749ea8
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 10:22:40 +0300

    Created TourPackageGuide configuration

---
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/Data/Configurations/TourPackageGuideConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TravelAgency.Data.Models;
+
+namespace TravelAgency.Data.Configurations
+{
+    public class TourPackageGuideConfiguration : IEntityTypeConfiguration<TourPackageGuide>
+    {
+        public void Configure(EntityTypeBuilder<TourPackageGuide> builder)
+        {
+            builder.HasKey(e =>
+                new { e.TourPackageId, e.GuideId }
+            );
+        }
+    }
+}

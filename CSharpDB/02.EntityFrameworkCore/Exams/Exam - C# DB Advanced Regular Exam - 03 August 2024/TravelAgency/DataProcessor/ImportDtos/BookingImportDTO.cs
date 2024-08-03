46e400041e671b9d13865aff0110e5e02af8c55c
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 11:38:18 +0300

    Created booking import dto

---
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/ImportDtos/BookingImportDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using static TravelAgency.Core.ValidationConstants;
+
+namespace TravelAgency.DataProcessor.ImportDtos
+{
+    public class BookingImportDTO
+    {
+        [Required]
+        [JsonProperty(nameof(BookingDate))]
+        public string BookingDate { get; set; } = null!;
+
+        [Required]
+        [MinLength(CustomerFullNameMinLength)]
+        [MaxLength(CustomerFullNameMaxLength)]
+        [JsonProperty(nameof(CustomerName))]
+        public string CustomerName { get; set; } = null!;
+
+        [Required]
+        [MinLength(TourPackageNameMinLength)]
+        [MaxLength(TourPackageNameMaxLength)]
+        [JsonProperty(nameof(TourPackageName))]
+        public string TourPackageName { get; set; } = null!;
+    }
+}

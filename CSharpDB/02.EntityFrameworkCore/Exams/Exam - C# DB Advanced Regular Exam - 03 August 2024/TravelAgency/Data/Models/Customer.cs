8641f8cf0c0902ea73be73ba91ed5bcb330d0119
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 10:22:10 +0300

    Created database models

---
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/Data/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TravelAgency.Core.ValidationConstants;
+
+namespace TravelAgency.Data.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(CustomerFullNameMaxLength)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(CustomerEmailMaxLength)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(CustomerPhoneNumberMaxLength)]
+        [RegularExpression(CustomerPhoneNumberRegex)]
+        public string PhoneNumber { get; set; } = null!;
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}

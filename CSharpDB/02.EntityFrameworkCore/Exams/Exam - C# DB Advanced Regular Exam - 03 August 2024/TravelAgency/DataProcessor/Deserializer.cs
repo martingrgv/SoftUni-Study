fc8027e715da8e3eddbdd902a16016ec8fe97efb
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 11:03:02 +0300

    Implemented customer deserialize method

--- CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/Deserializer.cs
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/Deserializer.cs
@@ -1,5 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Cadastre.Utilities;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 using TravelAgency.Data;
+using TravelAgency.Data.Models;
+using TravelAgency.DataProcessor.ImportDtos;
 
 namespace TravelAgency.DataProcessor
 {
@@ -12,9 +17,53 @@ namespace TravelAgency.DataProcessor
 
         public static string ImportCustomers(TravelAgencyContext context, string xmlString)
         {
-            throw new NotImplementedException();
-        }
+            var customerDTOs = XmlSerializerHelper.Deserialize<CustomerImportDTO[]>(xmlString, "Customers");
 
+            if (customerDTOs == null)
+            {
+                throw new InvalidOperationException("Could not extract data from XML!");
+            }
+
+            var strBuilder = new StringBuilder();
+            var customers = new List<Customer>();
+            var existingCustomers = context.Customers
+                .AsNoTracking()
+                .ToList();
+
+            foreach (var dto in customerDTOs)
+            {
+                if (!IsValid(dto))
+                {
+                    strBuilder.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (CustomersExist(customers, dto) ||
+                    CustomersExist(existingCustomers, dto))
+                {
+                    strBuilder.AppendLine(DuplicationDataMessage);
+                    continue;
+                }
+
+                var customer = new Customer
+                {
+                    FullName = dto.FullName,
+                    Email = dto.Email,
+                    PhoneNumber = dto.PhoneNumber
+                };
+
+                customers.Add(customer);
+                strBuilder.AppendLine(string.Format(
+                    SuccessfullyImportedCustomer, customer.FullName
+                ));
+            }
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return strBuilder.ToString().TrimEnd();
+        }
+        
         public static string ImportBookings(TravelAgencyContext context, string jsonString)
         {
             throw new NotImplementedException();
@@ -29,10 +78,18 @@ namespace TravelAgency.DataProcessor
 
             foreach (var validationResult in validationResults)
             {
-                string currValidationMessage = validationResult.ErrorMessage;
+                string currValidationMessage = validationResult.ErrorMessage!;
             }
 
             return isValid;
+        }
+
+        private static bool CustomersExist(List<Customer> customers, CustomerImportDTO customerDTO)
+        {
+            return customers.Any(c =>
+                    c.FullName == customerDTO.FullName ||
+                    c.Email == customerDTO.Email ||
+                    c.PhoneNumber == customerDTO.PhoneNumber);
         }
     }
 }

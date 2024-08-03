e9c363dfc0014523ed0e701496b64de4bd7e2fa4
martinwlsngrgv <martin.geor04@gmail.com>
2024-08-03 11:56:19 +0300

    Implemented guides export method

--- CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/Serializer.cs
+++ CSharpDB/02.EntityFrameworkCore/Exams/Exam - C# DB Advanced Regular Exam - 03 August 2024/TravelAgency/DataProcessor/Serializer.cs
@@ -1,4 +1,7 @@
-﻿using TravelAgency.Data;
+﻿using Cadastre.Utilities;
+using TravelAgency.Data;
+using TravelAgency.Data.Models.Enums;
+using TravelAgency.DataProcessor.ExportDtos;
 
 namespace TravelAgency.DataProcessor
 {
@@ -6,7 +9,26 @@ namespace TravelAgency.DataProcessor
     {
         public static string ExportGuidesWithSpanishLanguageWithAllTheirTourPackages(TravelAgencyContext context)
         {
-            throw new NotImplementedException();
+            var guideDTOs = context.Guides
+                .Where(g => g.Language == Language.Spanish)
+                .Select(g => new GuideExportDTO
+                {
+                    FullName = g.FullName,
+                    TourPackages = g.TourPackagesGuides.Select(tpg => new TourPackageExportDTO
+                    {
+                        Name = tpg.TourPackage.PackageName,
+                        Description = tpg.TourPackage.Description,
+                        Price = tpg.TourPackage.Price
+                    })
+                    .OrderByDescending(tp => tp.Price)
+                    .ThenBy(tp => tp.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(g => g.TourPackages.Length)
+                .ThenBy(g => g.FullName)
+                .ToArray();
+
+            return XmlSerializerHelper.Serialize(guideDTOs, "Guides");
         }
 
         public static string ExportCustomersThatHaveBookedHorseRidingTourPackage(TravelAgencyContext context)

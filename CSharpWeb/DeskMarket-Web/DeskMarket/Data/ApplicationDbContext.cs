e14098f0eac75510e492c453f0128679072debd6
Martin Georgiev <martin.geor04@gmail.com>
2024-10-19 09:09:47 +0300

    CSharp-Web: added DeskMarket exam proj

---
+++ CSharpWeb/DeskMarket-Web/DeskMarket/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeskMarket.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //base.OnModelCreating(builder);
+
+            //builder
+            //    .Entity<Category>()
+            //    .HasData(
+            //        new Category { Id = 1, Name = "Laptops" },
+            //        new Category { Id = 2, Name = "Workstations" },
+            //        new Category { Id = 3, Name = "Accessories" },
+            //        new Category { Id = 4, Name = "Desktops" },
+            //        new Category { Id = 5, Name = "Monitors" });
+        }
+    }
+}
